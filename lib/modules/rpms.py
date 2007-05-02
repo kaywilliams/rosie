be35074b03578a5c5ed99e5f35e37385b5690fbf
dmusgrave <devnull@localhost>
2007-05-02 15:08:15 -0700

    [svn] dimsbuild/lib/modules/rpms.py  * initial commit of rpms module

---
+++ lib/modules/rpms.py
@@ -0,0 +1,63 @@
+import os
+
+from os.path  import join
+from StringIO import StringIO
+
+import dims.filereader as filereader
+import dims.osutils    as osutils
+import dims.shlib      as shlib
+
+from event     import EVENT_TYPE_META
+from interface import EventInterface
+
+API_VERSION = 3.0
+
+EVENTS = [
+  {
+    'id': 'RPMS',
+    'provides': ['RPMS'],
+    'requires': ['.discinfo'], #!
+    'interface': 'RPMInterface',
+    'properties': EVENT_TYPE_META,
+  },
+]
+
+STORE_XML = ''' 
+<store id="dimsbuild-local">
+  <path>file://%s</path>
+</store>
+'''
+
+class RPMInterface(EventInterface):
+  def __init__(self, base):
+    EventInterface.__init__(self, base)
+    self.LOCAL_REPO = join(self.getMetadata(), 'localrepo/')
+  
+  def addRPM(self, path):
+    osutils.cp(path, self.LOCAL_REPO)
+  
+  def createrepo(self):
+    pwd = os.getcwd()
+    os.chdir(self.LOCAL_REPO)
+    shlib.execute('/usr/bin/createrepo -q .')
+    os.chdir(pwd)
+
+def prestores_hook(interface):
+  localrepo = join(interface.getMetadata(), 'localrepo/')
+  osutils.mkdir(localrepo)
+  interface.add_store(STORE_XML % localrepo)
+
+def postRPMS_hook(interface):
+  interface.createrepo()
+
+def postrepogen_hook(interface):
+  cfgfile = interface.getFlag('repoconfig')
+  if not cfgfile: return  
+  
+  lines = filereader.read(cfgfile)
+  
+  lines.append('[dimsbuild-local]')
+  lines.append('name = dimsbuild-local')
+  lines.append('baseurl = file://%s' % join(interface.getMetadata(), 'localrepo/'))
+  
+  filereader.write(lines, cfgfile)
