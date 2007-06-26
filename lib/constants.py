5db048396eabedb01488de0d004ecc8c6114be8c
dmusgrave <devnull@localhost>
2007-06-26 09:57:38 -0700

    [svn] dimsbuild/lib/constants.py  * initial commit of several useful constants values dimsbuild/lib/misc.py  * initial commit of several useful miscellaneous functions

---
+++ lib/constants.py
@@ -0,0 +1,6 @@
+
+BOOLEANS_TRUE  = ['True', 'true', 'Yes', 'yes', '1']
+BOOLEANS_FALSE = ['False', 'false', 'No', 'no', '0']
+
+OPT_FORCE = '--force'
+OPT_SKIP  = '--skip'
---
+++ lib/misc.py
@@ -0,0 +1,24 @@
+from StringIO import StringIO
+
+from dims import xmltree
+from dims import imerge
+
+def locals_imerge(string, ver):
+  tree = xmltree.read(StringIO(string))
+  locals = xmltree.Element('locals')
+  for child in tree.getroot().getchildren():
+    locals.append(imerge.incremental_merge(child, ver))
+  return locals
+
+def locals_printf(elem, vars):
+  string = elem.get('string-format/@string', elem.text)
+  format = elem.xpath('string-format/format/item/text()')
+  return printf(string, format, vars)
+
+def printf(string, fmt, vars):
+  for f in fmt:
+    try:
+      string = string.replace('%s', vars[f], 1)
+    except KeyError:
+      raise KeyError, "Variable '%s' not defined in supplied scope" % f
+  return string
