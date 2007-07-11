827d7729678adefee7d1c8c99b186d005a5e7106
uprakash <devnull@localhost>
2007-07-11 13:59:14 -0700

    [svn] * dimsbuild/trunk/lib/dimsbuild/modules/clean.py: - added a CleanInterface that stores the dispatch attribute for its   hooks to use. This is so that the hooks don't have to do   'interface._base.dispatch' to get a hold of the dispatch object.

--- lib/dimsbuild/modules/clean.py
+++ lib/dimsbuild/modules/clean.py
@@ -14,7 +14,8 @@ EVENTS = [
     'id': 'clean',
     'properties': event.EVENT_TYPE_MDLR|event.EVENT_TYPE_PROC,
     'conditional-requires': ['applyopt'],
-    'parent': 'ALL',    
+    'parent': 'ALL',
+    'interface': 'CleanInterface',
   },
 ]
 
@@ -59,6 +60,12 @@ class HookHandler:
     self.diffdict.update(diff)
     return self.diffdict
 
+#------------- INTERFACES -----------#
+class CleanInterface(EventInterface):
+  def __init__(self, base):
+    EventInterface.__init__(self, base)
+    self.dispatch = self._base.dispatch
+
 #------------- HOOKS --------------#
 class CleanHook(DiffMixin):
   def __init__(self, interface):
@@ -69,7 +76,7 @@ class CleanHook(DiffMixin):
     self.hookInfo = {}
 
     self.interface = interface
-    self.dispatch = self.interface._base.dispatch
+    self.dispatch = self.interface.dispatch
     
     DiffMixin.__init__(self, join(self.interface.METADATA_DIR, 'clean.md'), self.DATA)
     self._add_handler()
@@ -82,27 +89,27 @@ class CleanHook(DiffMixin):
 
   def force(self):
     self.clean_metadata()
-    # HACK ALERT: The following should ideally be in a hook on the ALL event,
-    # but that's not possible because we want applyopt to run before we
-    # delete the working directories.
+    ## HACK ALERT: The following should ideally be in a hook on the ALL event,
+    ## but that's not possible because we want applyopt to run before we
+    ## delete the working directories.
+    self.interface.log(0, "cleaning events")
+    self.interface.log(1, "cleaning all events")    
     if self.interface.isForced('ALL'):
-      self.interface.log(0, "cleaning events")
-      self.interface.log(1, "cleaning all events")
-      for dir in [self.interface.DISTRO_DIR]:
-        if exists(dir):
-          self.interface.log(2, "Removing directory '%s'" % dir)
-          osutils.rm(dir, recursive=True, force=True)
+      if exists(self.interface.DISTRO_DIR):
+        self.interface.log(2, "Removing directory '%s'" % self.interface.DISTRO_DIR)
+        osutils.rm(self.interface.DISTRO_DIR, recursive=True, force=True)
     
   def check(self):
     return self.test_diffs()
   
   def run(self):
-    # Look at previous HACK ALERT
+    ## Look at previous HACK ALERT
     for dir in [self.interface.SOFTWARE_STORE, self.interface.METADATA_DIR]:
       if not exists(dir):
         self.interface.log(2, "Making directory '%s'" % dir)
         osutils.mkdir(dir, parent=True)
-    
+    ## END HACK
+        
     if self.handlers['hooks'].diffdict.has_key(self.ID):
       self._force_all_events()
     else:
@@ -128,7 +135,7 @@ class CleanHook(DiffMixin):
     e = self.dispatch.get(eventid, err=True)
     if e.test(event.PROP_CAN_DISABLE):
       if not e.status:
-        self.interface.log(3, "forcing %s" % eventid)        
+        self.interface.log(2, "forcing %s" % eventid)        
         e.status = True
       self.interface._base.autoFC[e.id] = True
       if e.test(event.PROP_META):
