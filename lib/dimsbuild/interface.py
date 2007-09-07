09ede0b9b45ebcf7d6f0f23072591bcfc98fcad5
uprakash <devnull@localhost>
2007-09-07 12:20:50 -0700

    [svn] * lib/dimsbuild/interface.py: - backed out changes to EventInterface._setup_sync() because the directory   to which the files are to be downloaded/copied might have files from   other events. Therefore, the event can't own the entire directory.

--- lib/dimsbuild/interface.py
+++ lib/dimsbuild/interface.py
@@ -214,7 +214,7 @@ class EventInterface:
         src = P(s)
         dst = dst / d.lstrip('/')
         inputs.append(src)
-        outputs.append(self._setup_sync(src, dst, id or x))
+        outputs.extend(self._setup_sync(src, dst, id or x))
     
     for s in paths:
       assert isinstance(s, str)
@@ -222,22 +222,25 @@ class EventInterface:
         s = iprefix / s
       src = P(s)
       inputs.append(src)
-      outputs.append(self._setup_sync(src, dst, id or s))
+      outputs.extend(self._setup_sync(src, dst, id or s))
     
     return inputs, outputs
   
   def _setup_sync(self, sourcefile, dstdir, id):
     if not sourcefile.exists():
       raise IOError("missing input file(s) %s" % sourcefile)
+    rtn = []
     self.handlers['input'].idata.append(sourcefile)
-    if not self.syncinfo.has_key(id):
-      self.syncinfo[id] = {}
-    if not self.syncinfo[id].has_key(sourcefile):
-      self.syncinfo[id][sourcefile] = []
-    ofile = dstdir / sourcefile.basename
-    self.syncinfo[id][sourcefile].append(ofile)
-    self.handlers['output'].odata.append(ofile)
-    return ofile
+    for f in sourcefile.findpaths(type=pps.constants.TYPE_NOT_DIR):
+      if not self.syncinfo.has_key(id):
+        self.syncinfo[id] = {}
+      if not self.syncinfo[id].has_key(f):
+        self.syncinfo[id][f] = []
+      ofile = dstdir / f.tokens[len(sourcefile.tokens)-1:]
+      self.syncinfo[id][f].append(ofile)
+      self.handlers['output'].odata.append(ofile)
+      rtn.append(ofile)
+    return rtn
   
   def remove_output(self, rmlist=None, all=False, cb=None):
     """ 
