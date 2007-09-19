353986ea58a2de8be599b4fccc5e5c62e6db238d
kwilliams <devnull@localhost>
2007-09-19 09:05:25 -0700

    [svn] (continuing from previous partial commit) Summary: Separated software event into three events: download, gpgcheck, and gpgsign.  The primary benefit is that if checking or signing fail, the the events can correctly calculate newsign and newcheck vars on subsequent runs without needing to clear downloaded rpms from the builddata folder and start again.  Created software module that contains these three events, plus the createrepo event. Combined gpgsign-setup and gpgsign events into a single file.  Moved gpgsign homedir creation, as well as config validation and key download, from gpg-setup to gpgsign event. The gpg-setup event simply sets cvars for use by release-rpm and gpgsign events.

---
+++ lib/dimsbuild/modules/software/createrepo.py
@@ -0,0 +1,57 @@
+import os
+
+from dims import shlib
+
+from dimsbuild.event     import Event
+
+API_VERSION = 5.0
+
+class CreaterepoEvent(Event):
+  def __init__(self):
+    Event.__init__(self,
+      id = 'createrepo',
+      provides = ['rpms', 'rpms-directory', 'repodata-directory'],
+      requires = ['cached-rpms'],
+      conditionally_requires = ['comps-file', 'gpgsign-enabled', 'signed-rpms'],
+    )
+    
+    self.cvars['repodata-directory'] = self.SOFTWARE_STORE/'repodata'
+    
+    self.DATA = {
+      'input':   [],
+      'output':  [self.cvars['repodata-directory']]
+    }
+    
+  def setup(self):
+    self.setup_diff(self.DATA)
+
+    self.cvars['rpms-directory'] = self.SOFTWARE_STORE/self.cvars['base-vars']['product']
+
+    if self.cvars['gpgsign-enabled']:
+      self.setup_sync(self.cvars['rpms-directory'], paths=self.cvars['signed-rpms'])
+    else:
+      self.setup_sync(self.cvars['rpms-directory'], paths=self.cvars['cached-rpms'])
+  
+  def run(self):
+    self.log(0, "running createrepo")
+    
+    self.remove_output()
+    self.sync_input(copy=True, link=True)
+
+    # run createrepo
+    self.log(1, "running createrepo")
+    pwd = os.getcwd()
+    os.chdir(self.SOFTWARE_STORE)
+    shlib.execute('/usr/bin/createrepo -q -g %s .' % self.cvars['comps-file'])
+    os.chdir(pwd)
+    
+    self.write_metadata()
+  
+  def apply(self):
+    self.cvars['rpms'] = self.list_output()
+
+  def error(self, e):
+    # why?
+    self.clean()
+
+EVENTS = {'SOFTWARE': [CreaterepoEvent]}
