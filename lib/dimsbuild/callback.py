a19898677f25defc5ff132e14889ff8e2750f364
dmusgrave <devnull@localhost>
2007-10-11 12:36:49 -0700

    [svn] dimsbuild/lib/dimsbuild/callback.py  * progressbar.py layout and API updates

--- lib/dimsbuild/callback.py
+++ lib/dimsbuild/callback.py
@@ -36,7 +36,7 @@ class BuildSyncCallback(CachedSyncCallback):
     self.relpath = relpath
 
     if not self.logger.test(3):
-      self.bar.fo = None # turn off progressbar output
+      self.bar._fo = None # turn off progressbar output
 
   # sync callbacks - kinda hackish
   def start(self, src, dest):
@@ -72,25 +72,29 @@ class BuildDepsolveCallback:
     self.loop = 1
     self.count = 0
     self.bar = None
-  def pkgAdded(self, pkgtup=None, state=None):
-    if self.logger.test(2):
-      self.bar.update(self.bar.position+1)
-      self.bar.draw()
+  
   def start(self):
     pass
+  
   def tscheck(self, unresolved=0):
     self.count = unresolved
     if self.logger.test(2):
       if self.count == 1: msg = 'loop %d (%d package)'
       else:               msg = 'loop %d (%d packages)'
-      self.bar = ProgressBar(self.count, L2(msg % (self.loop, self.count)))
-      self.bar.layout = '[title:width=28] [ratio:width=9] [bar] [percent] [time]'
+      self.bar = ProgressBar(size=self.count, title=L2(msg % (self.loop, self.count)),
+                             layout='%(title)-28.28s %(ratio)9.9s [%(bar)s] %(time-elapsed)s',
+                             throttle=10)
       self.bar.start()
-      self.bar.draw()
+  
+  def pkgAdded(self, pkgtup=None, state=None):
+    if self.logger.test(2):
+      self.bar.status.position += 1
+  
   def restartLoop(self):
     if self.logger.test(2):
-      self.logger.logfile.log(2, str(self.bar))
       self.bar.finish()
+      self.logger.logfile.log(2, str(self.bar))
     self.loop += 1
+  
   def end(self):
     self.logger.log(2, 'pkglist resolution complete')
