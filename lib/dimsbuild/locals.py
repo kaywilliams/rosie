44349f58149b012f5c31e20f3285a0b7c7c2e235
kwilliams <devnull@localhost>
2007-10-12 13:01:43 -0700

    [svn] dimsbuild/trunk/lib/dimsbuild/locals.py * added back install class for anaconda 11.1.0.7-1, this is needed for fedora 6-based distros

--- lib/dimsbuild/locals.py
+++ lib/dimsbuild/locals.py
@@ -145,7 +145,39 @@ LOGOS_LOCALS = LocalsDict({
 })
 
 INSTALLCLASS_LOCALS = LocalsDict({
-  '0': # 11.1.2.36-1
+  '0': # 11.1.0.7-1
+''' 
+from installclass import BaseInstallClass
+from rhpl.translate import N_
+from constants import *
+
+class InstallClass(BaseInstallClass):
+  id = "custom"
+  name = N_("_Custom")
+  pixmap = "custom.png"
+  description = N_("Select the software you would like to install on your system.")
+  sortPriority = 10000
+  showLoginChoice = 1
+  showMinimal = 1
+
+  tasks = [("Default", %(default_groups)s), ("Everything", %(all_groups)s)]
+
+  def setInstallData(self, anaconda):
+    BaseInstallClass.setInstallData(self, anaconda)
+    BaseInstallClass.setDefaultPartitioning(self, anaconda.id.partitions, CLEARPART_TYPE_LINUX)
+
+  def setGroupSelection(self, anaconda):
+    grps = anaconda.backend.getDefaultGroups()
+    map(lambda x: anaconda.backend.selectGroup(x), grps)
+
+  def setSteps(self, dispatch):
+    BaseInstallClass.setSteps(self, dispatch);
+    dispatch.skipStep("partition")
+
+  def __init__(self, expert):
+    BaseInstallClass.__init__(self, expert)
+''',
+  '11.1.2.36-1': 
 '''
 from installclass import BaseInstallClass
 from rhpl.translate import N_
