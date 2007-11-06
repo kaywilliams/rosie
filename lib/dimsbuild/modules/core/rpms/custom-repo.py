52f37a65ca8f3ef49ed7e2e1dcdec84c43563bd0
Kay Williams <kwilliams@abodiosoftware.com>
2007-11-06 15:48:32 -0800

    custom-repo: renamed 'localrepo' module/event -> 'custom-repo

--- lib/dimsbuild/modules/core/rpms/custom-repo.py
+++ lib/dimsbuild/modules/core/rpms/custom-repo.py
@@ -7,19 +7,20 @@ from dimsbuild.logging import L1, L2
 from dimsbuild.repo    import RepoContainer
 
 API_VERSION = 5.0
-EVENTS = {'rpms': ['LocalRepoEvent']}
+EVENTS = {'rpms': ['CustomRepoEvent']}
 
-class LocalRepoEvent(Event):
+class CustomRepoEvent(Event):
   def __init__(self):
     Event.__init__(self,
-      id='localrepo',
+      
+      id='custom-repo',
       conditionally_requires=['custom-rpms', 'custom-srpms', 'custom-rpms-info'],
       provides=['repos', 'source-repos', 'included-packages', 'excluded-packages']
     )
     self.rc = RepoContainer(self)
 
-    self.LOCAL_RPMS  = self.mddir/'RPMS'
-    self.LOCAL_SRPMS = self.mddir/'SRPMS'
+    self.CUSTOM_RPMS  = self.mddir/'RPMS'
+    self.CUSTOM_SRPMS = self.mddir/'SRPMS'
 
     self.DATA = {
       'input':  [],
@@ -29,23 +30,23 @@ class LocalRepoEvent(Event):
   def setup(self):
     self.diff.setup(self.DATA)
 
-    self.io.setup_sync(self.LOCAL_RPMS,  paths=self.cvars['custom-rpms'],
-                       id='LOCAL_RPMS')
-    self.io.setup_sync(self.LOCAL_SRPMS, paths=self.cvars['custom-srpms'],
-                       id='LOCAL_SRPMS')
+    self.io.setup_sync(self.CUSTOM_RPMS,  paths=self.cvars['custom-rpms'],
+                       id='CUSTOM_RPMS')
+    self.io.setup_sync(self.CUSTOM_SRPMS, paths=self.cvars['custom-srpms'],
+                       id='CUSTOM_SRPMS')
 
-    self.DATA['output'].append(self.LOCAL_RPMS/'repodata')
-    self.DATA['output'].append(self.LOCAL_SRPMS/'repodata')
+    self.DATA['output'].append(self.CUSTOM_RPMS/'repodata')
+    self.DATA['output'].append(self.CUSTOM_SRPMS/'repodata')
 
-    self.rc.add_repo('localrepo', name='localrepo', baseurl=self.LOCAL_RPMS)
-    self.rc['localrepo'].localurl = self.LOCAL_RPMS
-    self.rc['localrepo'].pkgsfile = self.LOCAL_RPMS/'packages'
-    self.DATA['output'].append(self.rc['localrepo'].pkgsfile)
+    self.rc.add_repo('custom-repo', name='custom-repo', baseurl=self.CUSTOM_RPMS)
+    self.rc['custom-repo'].localurl = self.CUSTOM_RPMS
+    self.rc['custom-repo'].pkgsfile = self.CUSTOM_RPMS/'packages'
+    self.DATA['output'].append(self.rc['custom-repo'].pkgsfile)
 
-    self.rc.add_repo('localrepo-sources', name='localrepo-sources', baseurl=self.LOCAL_SRPMS)
-    self.rc['localrepo-sources'].localurl = self.LOCAL_SRPMS
-    self.rc['localrepo-sources'].pkgsfile = self.LOCAL_SRPMS/'packages'
-    self.DATA['output'].append(self.rc['localrepo-sources'].pkgsfile)
+    self.rc.add_repo('custom-repo-sources', name='custom-repo-sources', baseurl=self.CUSTOM_SRPMS)
+    self.rc['custom-repo-sources'].localurl = self.CUSTOM_SRPMS
+    self.rc['custom-repo-sources'].pkgsfile = self.CUSTOM_SRPMS/'packages'
+    self.DATA['output'].append(self.rc['custom-repo-sources'].pkgsfile)
     
     self.cvars.setdefault('repos', {})
     self.cvars.setdefault('source-repos', {})
@@ -58,22 +59,22 @@ class LocalRepoEvent(Event):
     self.log(1, L1("copying packages"))
     backup = self.files_callback.sync_start
     self.files_callback.sync_start = lambda : self.log(4, L1("RPMS"))
-    self.io.sync_input(link=True, what='LOCAL_RPMS')
+    self.io.sync_input(link=True, what='CUSTOM_RPMS')
     self.files_callback.sync_start = lambda : self.log(4, L1("SRPMS"))
-    self.io.sync_input(link=True, what='LOCAL_SRPMS')
+    self.io.sync_input(link=True, what='CUSTOM_SRPMS')
     self.files_callback.sync_start = backup
 
     self.log(1, L1("running createrepo"))
     if self.cvars['custom-rpms']:
       self.log(4, L2("RPMS"))
-      self._createrepo(self.LOCAL_RPMS)
-      self.rc['localrepo'].update_metadata()
-      self.rc['localrepo'].write_repo_content(self.rc['localrepo'].pkgsfile)
+      self._createrepo(self.CUSTOM_RPMS)
+      self.rc['custom-repo'].update_metadata()
+      self.rc['custom-repo'].write_repo_content(self.rc['custom-repo'].pkgsfile)
     if self.cvars['custom-srpms']:
       self.log(4, L2("SRPMS"))
-      self._createrepo(self.LOCAL_SRPMS)
-      self.rc['localrepo-sources'].update_metadata()
-      self.rc['localrepo-sources'].write_repo_content(self.rc['localrepo'].pkgsfile)
+      self._createrepo(self.CUSTOM_SRPMS)
+      self.rc['custom-repo-sources'].update_metadata()
+      self.rc['custom-repo-sources'].write_repo_content(self.rc['custom-repo'].pkgsfile)
 
     self.diff.write_metadata()
 
@@ -81,12 +82,12 @@ class LocalRepoEvent(Event):
     self.io.clean_eventcache()
     self._populate()
     if self.cvars['custom-rpms']:
-      self.rc['localrepo'].update_metadata()
-      self.cvars['repos']['localrepo'] = self.rc['localrepo']
+      self.rc['custom-repo'].update_metadata()
+      self.cvars['repos']['custom-repo'] = self.rc['custom-repo']
     if self.cvars['custom-srpms'] and self.cvars['source-repos']:
-      self.rc['localrepo-sources'].update_metadata()
-      self.cvars['source-repos']['localrepo-sources'] = \
-        self.rc['localrepo-sources']
+      self.rc['custom-repo-sources'].update_metadata()
+      self.cvars['source-repos']['custom-repo-sources'] = \
+        self.rc['custom-repo-sources']
 
   #----- HELPER METHODS -----#
   def _createrepo(self, path):
