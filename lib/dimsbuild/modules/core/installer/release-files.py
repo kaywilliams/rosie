5d7f15614a4dc397d521e68a5e5e0740d98697e4
Uday Prakash <uprakash@abodiosoftware.com>
2007-11-19 14:46:41 -0800

    release-files: using sync_input() to sync <path/> files.
    * lib/dimsbuild/modules/core/installer/release-files.py:
    - using IOMixin.sync_input() to get the relative paths working.

--- lib/dimsbuild/modules/core/installer/release-files.py
+++ lib/dimsbuild/modules/core/installer/release-files.py
@@ -17,18 +17,19 @@ class ReleaseFilesEvent(Event, ExtractMixin):
       requires = ['rpms-directory'],
       conditionally_comes_after = ['gpgsign'],
     )
-    
+
     self.DATA = {
       'variables': ['product'],
       'config':    ['.'],
       'input' :    [],
       'output':    [],
     }
-    
+
   def setup(self):
     self.DATA['input'].extend(self._find_rpms())
     self.diff.setup(self.DATA)
-    
+    self.io.setup_sync(self.SOFTWARE_STORE, xpaths=['path'], id='release-files-input')
+
   def run(self):
     self._extract()
 
@@ -36,27 +37,18 @@ class ReleaseFilesEvent(Event, ExtractMixin):
     self.io.clean_eventcache()
 
   def _generate(self, working_dir):
-    files = {}
+    self.io.sync_input(link=True, cache=False, what='release-files-input')
     rtn = []
-    for path in self.config.xpath('path', []):
-      source = P(path.text)
-      dest = self.SOFTWARE_STORE/path.attrib['dest']
-      files[source] = dest
     if self.config.get('include-in-tree/@use-default-set', 'True') \
            in BOOLEANS_TRUE:
       for default_item in ['eula.txt', 'beta_eula.txt', 'EULA', 'GPL', 'README',
                            '*-RPM-GPG', 'RPM-GPG-KEY-*', 'RPM-GPG-KEY-beta',
                            'README-BURNING-ISOS-en_US.txt', 'RELEASE-NOTES-en_US.html']:
         for item in working_dir.findpaths(glob=default_item):
-          files[item] = self.SOFTWARE_STORE
-    
-    for source in files.keys():
-      dest = files[source]
-      if source.isfile() and dest.isdir():
-        rtn.append(dest/source.basename)
-      self.link(source, dest)
+          self.link(item, self.SOFTWARE_STORE)
+          rtn.append(self.SOFTWARE_STORE / item.basename)
     return rtn
-  
+
   def _find_rpms(self):
     rpmnames = self.config.xpath('package/text()',
                                  [ '%s-release' % self.product ])
@@ -65,7 +57,7 @@ class ReleaseFilesEvent(Event, ExtractMixin):
       for rpm in self.cvars['rpms-directory'].findpaths(
           glob='%s-*-*' % rpmname, nregex=SRPM_REGEX):
         rpmset.add(rpm)
-    
+
     if not rpmset:
       for glob in ['*-release-*-[a-zA-Z0-9]*.[Rr][Pp][Mm]',
                    '*-release-notes-*-*']:
