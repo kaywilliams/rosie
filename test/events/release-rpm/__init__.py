0228681f412320be6bdfee524d9ae06e5dccec6a
Uday Prakash <uprakash@abodiosoftware.com>
2007-11-19 12:31:48 -0800

    added test case to check for RELEASE-NOTES in release-rpm.

--- test/events/release-rpm/__init__.py
+++ test/events/release-rpm/__init__.py
@@ -2,9 +2,10 @@ import unittest
 
 from dims import pps
 
-from test      import EventTestCase, EventTestRunner
+from test      import EventTestRunner
 from test.core import make_core_suite
-from test.rpms import RpmBuildMixinTestCase, InputFilesMixinTestCase, RpmCvarsTestCase
+from test.rpms import (RpmBuildMixinTestCase, InputFilesMixinTestCase,
+                       RpmCvarsTestCase, ExtractMixin, RpmEventTestCase)
 
 eventid = 'release-rpm'
 
@@ -77,6 +78,25 @@ class Test_ReleaseRpmCvars2(RpmCvarsTestCase):
                     self.event.cvars['custom-rpms-info'])
     self.failUnless(self.event.verifier.unittest().wasSuccessful())
 
+class Test_RNotesExistence(RpmEventTestCase, ExtractMixin):
+  def __init__(self, conf):
+    RpmEventTestCase.__init__(self, eventid, conf)
+    ExtractMixin.__init__(self)
+
+  def setUp(self):
+    RpmEventTestCase.setUp(self)
+
+  def tearDown(self):
+    if self.img_path:
+      self.img_path.rm(recursive=True, force=True)
+    RpmEventTestCase.tearDown(self)
+
+  def runTest(self):
+    self.tb.dispatch.execute(until=eventid)
+    self.failUnless(self.event.verifier.unittest().wasSuccessful())
+    rnotes = self.img_path.findpaths(glob='RELEASE-NOTES*')
+    self.failIf(len(rnotes) == 0)
+
 def make_suite(conf):
   suite = unittest.TestSuite()
   suite.addTest(make_core_suite(eventid, conf))
@@ -84,6 +104,7 @@ def make_suite(conf):
   suite.addTest(Test_ReleaseRpmBuild(conf))
   suite.addTest(Test_ReleaseRpmCvars1(conf))
   suite.addTest(Test_ReleaseRpmCvars2(conf))
+  suite.addTest(Test_RNotesExistence(conf))
   return suite
 
 def main(suite=None):
