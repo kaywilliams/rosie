2b3bca790496d0fd8d6d0cb1f2e5d3178a783913
Uday Prakash <uprakash@abodiosoftware.com>
2007-11-19 14:44:31 -0800

    more comprehensive test cases for release-files.

--- test/events/release-files/__init__.py
+++ test/events/release-files/__init__.py
@@ -7,49 +7,81 @@ from test.core import make_core_suite
 
 eventid = 'release-files'
 
-class Test_ReleaseEvent_Default(EventTestCase):
-  def __init__(self, conf):
+class ReleaseFilesEventTestCase(EventTestCase):
+  def __init__(self, conf, enabled='True'):
     EventTestCase.__init__(self, eventid, conf)
+    self.enabled = enabled
 
   def setUp(self):
     EventTestCase.setUp(self)
     self.clean_event_md()
-    xmllib.tree.Element('release-rpm', self.event._config, attrs={'enabled': 'False'})
+    xmllib.tree.Element('release-rpm', self.event._config, attrs={'enabled': self.enabled})
 
   def runTest(self):
-    self.tb.dispatch.execute(until=eventid)
-    self.failUnless(self.event.verifier.unittest().wasSuccessful())
+    self.tb.dispatch.execute(until='publish')
 
-class Test_ReleaseEvent_Custom(EventTestCase):
+class Test_ReleaseFiles(ReleaseFilesEventTestCase):
+  pass
+
+class Test_ReleaseFilesWithDefaultSet(ReleaseFilesEventTestCase):
+  def setUp(self):
+    ReleaseFilesEventTestCase.setUp(self)
+    rfiles = xmllib.tree.Element('release-files', self.event._config)
+    xmllib.tree.Element('include-in-tree', rfiles, attrs={'use-default-set': 'True'})
+
+class Test_ReleaseFilesWithDefaultSet(ReleaseFilesEventTestCase):
+  def setUp(self):
+    ReleaseFilesEventTestCase.setUp(self)
+    rfiles = xmllib.tree.Element('release-files', self.event._config)
+    xmllib.tree.Element('include-in-tree', rfiles, attrs={'use-default-set': 'False'})
+
+class Test_ReleaseFilesWithInputFiles(ReleaseFilesEventTestCase):
+  def setUp(self):
+    ReleaseFilesEventTestCase.setUp(self)
+    rfiles = xmllib.tree.Element('release-files', self.event._config)
+    xmllib.tree.Element('path', rfiles, text='input1')
+    xmllib.tree.Element('path', rfiles, text='input2', attrs={'dest': 'dir1/dir2'})
+    xmllib.tree.Element('path', rfiles, text='input3', attrs={'dest':'dir3'})
+
+  def runTest(self):
+    ReleaseFilesEventTestCase.runTest(self)
+    for file in [ self.event.SOFTWARE_STORE / 'input1',
+                  self.event.SOFTWARE_STORE / 'dir1/dir2/input2',
+                  self.event.SOFTWARE_STORE / 'dir3/input3']:
+      self.failUnless(file.exists())
+
+class Test_ReleaseFilesWithPackageElement(ReleaseFilesEventTestCase):
   def __init__(self, conf):
-    EventTestCase.__init__(self, eventid, conf)
+    ReleaseFilesEventTestCase.__init__(self, conf, 'True')
 
   def setUp(self):
-    EventTestCase.setUp(self)
-    self.clean_event_md()
-    xmllib.tree.Element('release-rpm', self.event._config, attrs={'enabled': 'True'})
-
-  def runTest(self):
-    self.tb.dispatch.execute(until=eventid)
-    self.failUnless(self.event.verifier.unittest().wasSuccessful())
+    ReleaseFilesEventTestCase.setUp(self)
+    rfiles = xmllib.tree.Element('release-files', self.event._config)
+    xmllib.tree.Element('package', rfiles, text='%s-release' % self.event.product)
 
 def make_suite(confdir):
   suite = unittest.TestSuite()
 
-  #suite.addTest(make_core_suite(eventid, confdir/'fedora6.conf'))
+  suite.addTest(make_core_suite(eventid, confdir/'fedora6.conf'))
+  for config in [ confdir / 'fedora6.conf',
+                  confdir / 'fedora7.conf',
+                  confdir / 'fedora8.conf',
+                  confdir / 'centos5.conf',
+                  confdir / 'redhat5.conf']:
+    # default run
+    suite.addTest(Test_ReleaseFiles(config, 'True'))
+    suite.addTest(Test_ReleaseFiles(config, 'False'))
 
-  suite.addTest(Test_ReleaseEvent_Default(confdir/'fedora6.conf'))
-  suite.addTest(Test_ReleaseEvent_Default(confdir/'fedora7.conf'))
-  suite.addTest(Test_ReleaseEvent_Default(confdir/'fedora8.conf'))
-  suite.addTest(Test_ReleaseEvent_Default(confdir/'centos5.conf'))
-  suite.addTest(Test_ReleaseEvent_Default(confdir/'redhat5.conf'))
+    # execution with modification of 'use-default-set' attribute
+    suite.addTest(Test_ReleaseFilesWithDefaultSet(config, 'True'))
+    suite.addTest(Test_ReleaseFilesWithDefaultSet(config, 'False'))
 
-  suite.addTest(Test_ReleaseEvent_Custom(confdir/'fedora6.conf'))
-  suite.addTest(Test_ReleaseEvent_Custom(confdir/'fedora7.conf'))
-  suite.addTest(Test_ReleaseEvent_Custom(confdir/'fedora8.conf'))
-  suite.addTest(Test_ReleaseEvent_Custom(confdir/'centos5.conf'))
-  suite.addTest(Test_ReleaseEvent_Custom(confdir/'redhat5.conf'))
+    # execution with <path/> element
+    suite.addTest(Test_ReleaseFilesWithInputFiles(config, 'True'))
+    suite.addTest(Test_ReleaseFilesWithInputFiles(config, 'False'))
 
+    # execution with <package/> element
+    suite.addTest(Test_ReleaseFilesWithPackageElement(config))
   return suite
 
 def main(suite=None):
