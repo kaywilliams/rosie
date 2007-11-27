8c0e86191b37d0634764c302e2a838dd96a13db6
Uday Prakash <uprakash@abodiosoftware.com>
2007-11-27 11:59:13 -0800

    config-rpm: removed debug statement

--- lib/dimsbuild/modules/extensions/rpms/config-rpm.py
+++ lib/dimsbuild/modules/extensions/rpms/config-rpm.py
@@ -68,7 +68,6 @@ class ConfigRpmEvent(Event, RpmBuildMixin, InputFilesMixin):
 
   def _getpscript(self):
     post_install_scripts = self.io.list_output(what='config-files')
-    print post_install_scripts
     if post_install_scripts:
       script = self.build_folder / 'post-install.sh'
       script.write_lines(
