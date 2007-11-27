2259f4f25f034793c8e8c0688cf16189b44d22ba
Uday Prakash <uprakash@abodiosoftware.com>
2007-11-27 12:20:32 -0800

    gpgsign: whitespace cleanup

--- lib/dimsbuild/modules/extensions/software/gpgsign.py
+++ lib/dimsbuild/modules/extensions/software/gpgsign.py
@@ -9,7 +9,7 @@ from dimsbuild.event     import Event
 from dimsbuild.logging   import L1, L2
 
 API_VERSION = 5.0
-EVENTS = {'setup': ['GpgSetupEvent'], 'software': ['GPGSignEvent']}
+EVENTS = {'setup': ['GpgSetupEvent'], 'software': ['GpgSignEvent']}
 
 P = pps.Path
 
@@ -22,24 +22,24 @@ class GpgSetupEvent(Event):
                   'gpgsign-secret-key',
                   'gpgsign-passphrase'],
     )
-  
+
   def apply(self):
     pubkey = self.config.get('gpg-public-key/text()', None)
     if pubkey: self.cvars['gpgsign-public-key'] = P(pubkey)
-    
+
     seckey = self.config.get('gpg-secret-key/text()', None)
     if seckey: self.cvars['gpgsign-secret-key'] = P(seckey)
-    
+
     if self.config.pathexists('gpg-passphrase'):
       self.cvars['gpgsign-passphrase'] = self.config.get('gpg-passphrase/text()', '')
-  
+
   def verify_cvars(self):
     "public and secret key cvars defined"
     self.verifier.failUnless(self.cvars['gpgsign-public-key'])
     self.verifier.failUnless(self.cvars['gpgsign-secret-key'])
 
 
-class GPGSignEvent(GpgMixin, Event):
+class GpgSignEvent(GpgMixin, Event):
   def __init__(self):
     Event.__init__(self,
       id = 'gpgsign',
@@ -48,29 +48,29 @@ class GPGSignEvent(GpgMixin, Event):
       conditionally_comes_after = ['gpgcheck'],
       provides = ['signed-rpms'],
     )
-    
+
     self.gpgsign_cb = GpgCallback(self.logger)
-    
+
     GpgMixin.__init__(self)
-    
+
     self.DATA = {
       'input':     [],
       'output':    [],
     }
-  
+
   def setup(self):
     self.diff.setup(self.DATA)
-    
+
     self.io.setup_sync(self.mddir, paths=self.cvars['gpgsign-public-key'], id='pubkey')
     self.io.setup_sync(self.mddir, paths=self.cvars['gpgsign-secret-key'], id='seckey')
-    
+
     self.io.setup_sync(self.mddir/'rpms', paths=self.cvars['cached-rpms'], id='rpms')
-  
+
   def run(self):
     self.log(1, L1("configuring gpg signing"))
     # sync keys
     newkeys = self.io.sync_input(what=['pubkey','seckey'], cache=True)
-    
+
     # import keys
     gnupg_dir = self.mddir / '.gnupg'
     pubkey = self.io.list_output(what='pubkey')[0]
@@ -81,20 +81,20 @@ class GPGSignEvent(GpgMixin, Event):
       self.import_key(gnupg_dir, pubkey)
       self.import_key(gnupg_dir, seckey)
     self.DATA['output'].append(gnupg_dir)
-    
+
     # don't leave secret key lying around
     seckey.remove()
-    
+
     # sync rpms to output folder
     self.log(1, L1("preparing to sign rpms"))
     newrpms = self.io.sync_input(what='rpms', cache=True)
-    
+
     # sign rpms
     if newkeys:
       signrpms = self.io.list_output(what='rpms')
     else:
       signrpms = newrpms
-    
+
     if signrpms:
       self.log(1, L1("signing rpms"))
       self.gpgsign_cb.start()
@@ -108,9 +108,9 @@ class GPGSignEvent(GpgMixin, Event):
         self.gpgsign_cb.pkgChecked(rpm.basename)
       self.gpgsign_cb.endRepo()
       self.gpgsign_cb.end()
-    
+
     self.diff.write_metadata()
-  
+
   def apply(self):
     self.io.clean_eventcache()
     self.cvars['signed-rpms'] = self.io.list_output(what='rpms')
