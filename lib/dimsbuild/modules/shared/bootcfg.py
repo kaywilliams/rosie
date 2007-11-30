7b3afab12f93a5b98409bc45f24f66cf178a2291
Daniel Musgrave <dmusgrave@abodiosoftware.com>
2007-11-30 15:50:01 -0800

    preliminary fix to bug 111 (isolinux.cfg/syslinux.cfg menu title)
    lib/dimsbuild/modules/shared/bootcfg.py
     * replaces 'menu title Welcome to .*!' with 'menu title Welcome to %s!' %
       Event.fullname

--- lib/dimsbuild/modules/shared/bootcfg.py
+++ lib/dimsbuild/modules/shared/bootcfg.py
@@ -15,7 +15,7 @@ class BootConfigDummy(object):
     self.ptr = ptr
     self.boot_args = None
     self._macros = {}
-  
+
   def setup(self, defaults=None):
     self.boot_args = self.ptr.config.get('boot-config/append-args/text()', '').split()
     if defaults:
@@ -25,19 +25,19 @@ class BootConfigDummy(object):
         self.boot_args.extend(defaults)
     if self.ptr.cvars['boot-args']:
       self.boot_args.append(self.cvars['boot-args'].split())
-    
+
     self.boot_args = [ self._expand_macros(x) for x in self.boot_args ]
-  
+
   def modify(self, dst, cfgfile=None):
     if not self.boot_args: return
-    
+
     boot_args = [ self._expand_macros(x) for x in self.boot_args ]
-    
+
     config = P(self.ptr.config.get('boot-config/file/text()',
                cfgfile or self.ptr.cvars['boot-config-file']))
     lines = config.read_lines()
     _label = False # have we seen a label line yet?
-    
+
     for i in range(0, len(lines)):
       tokens = lines[i].strip().split()
       if not tokens: continue
@@ -47,19 +47,21 @@ class BootConfigDummy(object):
         elif len(tokens) < 2: continue
         elif tokens[1] == '-': continue
         lines[i] = '%s %s' % (lines[i].rstrip(), ' '.join(boot_args))
-    
+      elif tokens[0] == 'menu' and tokens[1] == 'title':
+        lines[i] = 'Welcome to %s' % self.ptr.fullname
+
     dst.remove()
     dst.write_lines(lines)
-  
+
   def _expand_macros(self, s):
     for k,v in self._macros.items():
       s = s.replace(k, v)
     return s
-  
+
   def _process_method(self, args):
     if self.ptr.cvars['web-path']:
       args.append('method=%s/os' % self.ptr.cvars['web-path'])
-  
+
   def _process_ks(self, args):
     if self.ptr.cvars['ks-path']:
       args.append('ks=file:%s' % self.ptr.cvars['ks-path'])
