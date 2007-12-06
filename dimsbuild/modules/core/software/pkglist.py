69736e949ab15128ff4232666ae08b7518a8ef2a
Uday Prakash <uprakash@abodiosoftware.com>
2007-12-06 12:37:26 -0800

    pkglist: don't reinstall already installed package in transcation set.

--- dimsbuild/modules/core/software/pkglist.py
+++ dimsbuild/modules/core/software/pkglist.py
@@ -5,6 +5,7 @@ import yum
 
 from dims import difftest
 from dims import pps
+from dims import depsolver #!
 
 from dims.depsolver import DepSolver
 
@@ -84,28 +85,40 @@ class PkglistEvent(Event):
 
     # create pkglist
     self.log(1, L1("generating new package list"))
-    if not self.dsdir.exists(): self.dsdir.mkdirs()
+    if not self.dsdir.exists():
+      self.dsdir.mkdirs()
 
     repoconfig = self._create_repoconfig()
-    depsolve_file = self.mddir / 'depsolve-results'
-    todepsolve = copy.deepcopy(self.cvars.get('required-packages', []))
-    user_required = copy.deepcopy(self.cvars.get('user-required-packages', []))
-    removed_items = []
+    required_packages = self.cvars.get('required-packages', [])
+    user_required = self.cvars.get('user-required-packages', [])
+
+    toinstall = []
+    toremove = []
     diffdict = self.diff.handlers['variables'].diffdict
     if diffdict.has_key("cvars['required-packages']"):
       prev, curr = diffdict["cvars['required-packages']"]
-      if ( prev is not None and
-           not isinstance(prev, difftest.NoneEntry) and
-           not isinstance(prev, difftest.NewEntry) ):
-        removed_items.extend([ x for x in prev if x not in curr ])
+      if prev is None or \
+           isinstance(prev, difftest.NewEntry) or \
+           isinstance(prev, difftest.NoneEntry):
+        prev = []
+      if curr is None or \
+           isinstance(curr, difftest.NewEntry) or \
+           isinstance(curr, difftest.NoneEntry):
+        curr = []
+      if prev:
+        toremove.extend([ (x, None, None) for x in prev if x not in curr ])
+      if curr:
+        toinstall.extend([ (x, None, None) for x in curr if x not in prev ])
 
-    solver = IDepSolver(depsolve_file, todepsolve, removed_items, user_required,
-                        config=str(repoconfig), root=str(self.dsdir),
-                        arch=self.arch, callback=BuildDepsolveCallback(self.logger))
-    solver.setup()
+    depsolve_results = self.mddir / 'depsolve-results'
+    solver = IDepsolver(repoconfig, self.dsdir, self.arch, depsolve_results,
+                        BuildDepsolveCallback(self.logger))
+    solver.setup(required_packages, user_required, toremove, toinstall)
     solver.resolveDeps()
-    pkgtups = [ x.pkgtup for x in solver.tsInfo.getMembers() ]
-    solver.teardown()
+    solver.runTransaction()
+
+    pkgtups = [ x.pkgtup for x in \
+                solver.tsInfo.getMembers(None, yum.constants.TS_INSTALL_STATES) ]
 
     self.log(1, L1("pkglist closure achieved in %d packages" % len(pkgtups)))
 
@@ -117,10 +130,9 @@ class PkglistEvent(Event):
     self.log(1, L1("writing pkglist"))
     self.pkglistfile.write_lines(pkglist)
 
-    self.DATA['output'].extend([self.dsdir, self.pkglistfile, depsolve_file,
-                                repoconfig])
+    self.DATA['output'].extend([self.dsdir, self.pkglistfile,
+                                repoconfig, depsolve_results])
     self.diff.write_metadata()
-    solver = None
 
   def apply(self):
     self.io.clean_eventcache()
@@ -157,159 +169,58 @@ class PkglistEvent(Event):
     repoconfig.write_lines(conf)
     return repoconfig
 
-
-class IDepSolver(DepSolver):
-  def __init__(self, cache_file, todepsolve, removed_items, user_required,
-               config='/etc/yum.conf', root='/tmp/depsolver', arch=None, callback=None):
-    DepSolver.__init__(self, config=config, root=root,
+class IDepsolver(DepSolver):
+  def __init__(self, config, root, arch, depsolve_results, callback):
+    DepSolver.__init__(self, config=str(config), root=str(root),
                        arch=arch, callback=callback)
-    self.cache_file = P(cache_file)
-    self.depsolve_results = {}
-    self.depsolve_cache = {}
-    self.removed_items = removed_items
-    self.user_required = user_required
-    self.todepsolve = todepsolve
-    self.unresolved = set()
+    self.depsolve_results = depsolve_results
 
-  def setup(self):
+  def setup(self, required_packages, user_required, toremove, toinstall):
     DepSolver.setup(self)
-    if self.cache_file.exists():
-      f = self.cache_file.open()
-      self.depsolve_cache = pickle.load(f)
-      f.close()
-    self.filterCache()
-    for package in self.todepsolve:
+    if not toremove:
+      self.populateRpmDB()
+
+    for n,v,r in toremove:
+      self.remove(name=n, version=v, release=r)
+    for n,v,r in toinstall:
       try:
-        txmbrs = self.install(name=package)
-        for po in txmbrs:
-          self.unresolved.add((po, package))
-          self.depsolve_results.setdefault(package, set()).add(po.pkgtup)
+        self.install(name=n, version=v, release=r)
       except yum.Errors.InstallError, e:
-        if package in self.user_required:
-          raise DepSolveError("User required package '%s' not found" % package)
-
-  def resolveDeps(self):
-    "Resolve dependencies on all selected packages and groups."
-    self.initActionTs()
-    if self.dsCallback: self.dsCallback.start()
-    unresolved = self.unresolved
-    while len(unresolved) > 0:
-      if self.dsCallback:
-        self.dsCallback.tscheck(len(unresolved))
-      self.installFromCache(unresolved)
-      unresolved = self.tsCheck(unresolved)
-      if self.dsCallback:
-        self.dsCallback.restartLoop()
-    self.deps = {}
-    f = self.cache_file.open('w')
-    pickle.dump(self.depsolve_results, f)
-    f.close()
-
-  def tsCheck(self, tocheck):
-    "Attempt to resolve dependencies on items in tocheck."
-    unresolved = set()
-    for txmbr, pkgname in tocheck:
-      self.depsolve_results[txmbr.name] = set()
-
-      # some kind of speed optimization
-      if txmbr.name == 'redhat-lsb' and len(tocheck) > 2:
-        unresolved.add((txmbr, pkgname))
-        continue
-
-      if self.dsCallback:
-        self.dsCallback.pkgAdded(txmbr.po.pkgtup)
-
-      if txmbr.output_state not in yum.constants.TS_INSTALL_STATES:
-        continue
-
-      # get requires and provides for transaction member
-      reqs = txmbr.po.returnPrco('requires')
-      provs = txmbr.po.returnPrco('provides')
-
-      # process each requires
-      for req in reqs:
-        # skip rpmlib and config requires
-        if req[0].startswith('rpmlib(') or req[0].startswith('config('):
-          continue
-
-        # skip circular requires/provides
-        if req in provs:
-          continue
-
-        # figure out a package that provides the necessary capability
-        dep = self.findBestPackageByNfv(req)
-        if dep is None:
-          #raise DepSolveError("Unresolvable dependency %s for package %s"
-          #                    % (str(req), txmbr.name))
-          continue
-
-        self.depsolve_results[txmbr.name].add(dep.pkgtup)
-        self.depsolve_results.setdefault(pkgname, set()).add(dep.pkgtup)
-
-        # skip circular requires/provides, again
-        if txmbr.name == dep.name:
-          continue
-
-        if self.tsInfo.exists(dep.pkgtup):
-          # provider already exists in transaction info, get it
-          pkgs = self.tsInfo.getMembers(pkgtup=dep.pkgtup)
-          member = self.bestPackagesFromList(pkgs, arch=self.arch)[0]
-          self.depsolve_results.setdefault(pkgname, set()).update(
-            self.depsolve_results.get(dep.name, set())
-          )
+        if n in user_required:
+          raise
         else:
-          # select provider and add it to list of unresolved items
-          member = self.tsInfo.addInstall(dep)
-          unresolved.add((member, pkgname))
+          pass
+    updates_available = self.update()
+    if updates_available or toremove:
+      self.resetRpmDB()
+      for package in required_packages:
+        if package in toinstall:
+          continue
+        try:
+          self.install(name=package)
+        except yum.Errors.InstallError, e:
+          if package in user_required:
+            raise
+          else:
+            pass
 
-        # perform dependency mapping
-        found = False
-        for dependspo in txmbr.depends_on:
-          if member.po == dependspo:
-            found = True; break
-        if not found:
-          txmbr.setAsDep(member.po)
+  def resetRpmDB(self):
+    self.rpmdb = None
+    DepSolver.doRpmDBSetup(self)
 
-    # return list of new, unresolved packages
-    return unresolved
+  def populateRpmDB(self):
+    if self.depsolve_results.exists():
+      for n,a,e,v,r in pickle.load(self.depsolve_results.open('r')):
+        for po in self.pkgSack.searchNevra(name=n, arch=a, epoch=e, ver=v, rel=r):
+          self.rpmdb.addPackage(po)
 
-  def teardown(self):
-    DepSolver.teardown(self)
-    self.depsolve_results = None
-    self.depsolve_cache = None
-    self.unresolved = None
-
-  def installFromCache(self, unresolved):
-    installed = []
-    for txmbr, pkgname in unresolved:
-      if self.depsolve_cache.has_key(pkgname):
-        for pkgtup in self.depsolve_cache[pkgname]:
-          self.install(pkgtup=pkgtup)
-          self.depsolve_results.setdefault(pkgname, set()).add(pkgtup)
-        if self.dsCallback:
-          self.dsCallback.pkgAdded(txmbr.pkgtup)
-        installed.append((txmbr, pkgname))
-    for item in installed:
-      unresolved.remove(item)
-
-  def filterCache(self):
-    """
-    Remove items in cache that are not needed anymore.
-    """
-    for package in self.removed_items:
-      if self.depsolve_cache.has_key(package):
-        self.depsolve_cache.pop(package)
-
-    stale_data = set()
-    for package, deps in self.depsolve_cache.items():
-      for dep in deps:
-        best_package = self.findBestPackageByName(dep[0])
-        if best_package is None or \
-            best_package.pkgtup != dep:
-          stale_data.add(package)
-
-    for item in stale_data:
-      self.depsolve_cache.pop(item)
-
-#------ ERRORS ------#
-class DepSolveError(Exception): pass
+  def runTransaction(self):
+    DepSolver.runTransaction(self)
+    for po in self.rpmdb.returnPackages():
+      if not self.tsInfo.exists(pkgtup=po.pkgtup):
+        self.tsInfo.addInstall(po)
+    f = open(self.depsolve_results, 'w')
+    pickle.dump([ x.pkgtup for x in \
+                  self.tsInfo.getMembers(None, yum.constants.TS_INSTALL_STATES)
+                ], f)
+    f.close()
