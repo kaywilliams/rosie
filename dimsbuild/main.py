11b7dd372d251574b71af910146e68c9cfee042a
Uday Prakash <uprakash@abodiosoftware.com>
2007-12-07 17:02:28 -0800

    dimsbuild/main: setting default copyright to empty string
    * dimsbuild/main.py:
    - setting default copyright to empty string instead of None

--- dimsbuild/main.py
+++ dimsbuild/main.py
@@ -358,7 +358,7 @@ class Build(object):
     bv['webloc']   = Event._config.get(qstr % 'bug-url', 'No bug url provided')
     bv['pva']      = '%(product)s-%(version)s-%(basearch)s' % bv
     bv['product-path'] = Event._config.get(qstr % 'product-path', 'Packages')
-    bv['copyright'] = Event._config.get(qstr % 'copyright', None)
+    bv['copyright'] = Event._config.get(qstr % 'copyright', '')
 
     for k,v in bv.items():
       setattr(Event, k, v)
