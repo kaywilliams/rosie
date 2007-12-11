2a55ab75f08f0e40c881bce2efb1b364a55b958d
Daniel Musgrave <dmusgrave@abodiosoftware.com>
2007-12-11 15:21:58 -0800

    bug 111 (isolinux.cfg not modified in isolinux/ folder)
    dimsbuild/modules/core/installer/isolinux.py
     * isolinux event now explicitly modifies the isolinux.cfg file contained in
       the isolinux folder, modifying the title (if present) and appending the
       ks arg (if 'kickstart' event runs)

--- dimsbuild/modules/core/installer/isolinux.py
+++ dimsbuild/modules/core/installer/isolinux.py
@@ -1,16 +1,17 @@
 from dimsbuild.event   import Event
 
-from dimsbuild.modules.shared import FileDownloadMixin
+from dimsbuild.modules.shared import FileDownloadMixin, BootConfigMixin
 
 API_VERSION = 5.0
 EVENTS = {'installer': ['IsolinuxEvent']}
 
-class IsolinuxEvent(Event, FileDownloadMixin):
+class IsolinuxEvent(Event, FileDownloadMixin, BootConfigMixin):
   def __init__(self):
     Event.__init__(self,
       id = 'isolinux',
       provides = ['isolinux-files', 'boot-config-file'],
       requires = ['anaconda-version', 'source-vars', 'base-repoid'],
+      conditionally_requires = ['ks-path', 'boot-args'],
     )
 
     self.DATA = {
@@ -21,22 +22,29 @@ class IsolinuxEvent(Event, FileDownloadMixin):
     }
 
     FileDownloadMixin.__init__(self)
-  
+    BootConfigMixin.__init__(self)
+
   def setup(self):
     self.diff.setup(self.DATA)
+    boot_arg_defaults = []
+    self.bootconfig._process_ks(boot_arg_defaults)
+    self.bootconfig.setup(defaults=boot_arg_defaults)
     self.file_locals = self.locals.files['isolinux']
     FileDownloadMixin.setup(self)
-  
+
   def run(self):
     self._download()
+    self.bootconfig.modify(
+      self.SOFTWARE_STORE/self.file_locals['isolinux.cfg']['path'],
+      self.SOFTWARE_STORE/self.file_locals['isolinux.cfg']['path'])
     self.diff.write_metadata()
-  
+
   def apply(self):
     self.io.clean_eventcache()
-    
+
     self.cvars['isolinux-files'] = {}
     for k,v in self.file_locals.items():
       self.cvars['isolinux-files'][k] = self.SOFTWARE_STORE/v['path']
-    
+
     self.cvars['boot-config-file'] = \
       self.SOFTWARE_STORE/self.file_locals['isolinux.cfg']['path']
