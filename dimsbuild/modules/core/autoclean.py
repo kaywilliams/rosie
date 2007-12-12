66b665a27ff01768223108d3c02864cbe6ff032a
Uday Prakash <uprakash@abodiosoftware.com>
2007-12-11 16:34:49 -0800

    autoclean: use xmllib.config instead of xmllib.tree
    * dimsbuild/modules/core/autoclean.py:
    - using the xmllib.config module instead of xmllib.tree so that the
    metadata files are human-readable.

--- dimsbuild/modules/core/autoclean.py
+++ dimsbuild/modules/core/autoclean.py
@@ -13,18 +13,18 @@ class AutocleanEvent(Event):
       comes_after = ['init'],
       comes_before = ['setup'],
     )
-    
+
     self.DATA = {'events': {}}
     self.eventinfo = {}
-  
+
   def setup(self):
     for event in self._getroot():
       self.eventinfo[event.id] = event
       self.DATA['events'].update({event.id: str(event.event_version)})
-    
+
     self.diff.setup(self.DATA)
     self.diff.add_handler(EventHandler(self.DATA['events']))
-    
+
     # delete all the folders in the metadata directory that are from events
     # that aren't running this pass
     mdfolders = self.METADATA_DIR.listdir()
@@ -33,7 +33,7 @@ class AutocleanEvent(Event):
         mdfolders.remove(self.METADATA_DIR/event.id)
       except:
         pass
-    
+
     for mdfolder in mdfolders:
       self.log(4, L2("removing unused event metadata directory '%s'" % mdfolder.basename))
       mdfolder.rm(recursive=True, force=True)
@@ -44,33 +44,33 @@ class AutocleanEvent(Event):
       if prevver and currver:
         self.log(2, L1("forcing '%s'" % eventid))
         self.eventinfo[eventid].status = True
-    
+
     self.diff.write_metadata()
-    
-  
+
+
 #------ METADATA HANDLER ------#
 class EventHandler:
   def __init__(self, data):
     self.name = 'events'
-    
+
     self.data = data
     self.events = {}
     self.diffdict = {}
-  
+
   def clear(self):
     self.events.clear()
     self.diffdict.clear()
-        
+
   def mdread(self, metadata):
     for event in metadata.xpath('/metadata/events/event'):
       self.events[event.get('@id')] = event.get('version/text()')
-  
+
   def mdwrite(self, root):
-    parent = xmllib.tree.uElement('events', parent=root)
- 
+    parent = xmllib.config.uElement('events', parent=root)
+
     for k,v in self.data.items():
-      e = xmllib.tree.Element('event', parent=parent, attrs={'id': k})
-      xmllib.tree.Element('version', parent=e, text=str(v))
+      e = xmllib.config.Element('event', parent=parent, attrs={'id': k})
+      xmllib.config.Element('version', parent=e, text=str(v))
 
   def diff(self):
     for k,v in self.events.items():
@@ -80,7 +80,7 @@ class EventHandler:
           self.diffdict[k] = (v, newv)
       else:
         self.diffdict[k] = (v, None)
-    
+
     for k,v in self.data.items():
       if not self.events.has_key(k):
         self.diffdict[k] = (None, v)
