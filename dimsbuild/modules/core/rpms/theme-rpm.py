8bcc1049e6d4d48dd48a1a14ca925835f6a64058
Uday Prakash <uprakash@abodiosoftware.com>
2007-12-18 16:24:20 -0800

    theme-rpm: not added bdist-base to output files' list

--- dimsbuild/modules/core/rpms/theme-rpm.py
+++ dimsbuild/modules/core/rpms/theme-rpm.py
@@ -53,7 +53,6 @@ class ThemeRpmEvent(Event, RpmBuildMixin):
   def run(self):
     self.io.clean_eventcache(all=True)
     self._build_rpm()
-    self.DATA['output'].append(self.bdist_base)
     self.diff.write_metadata()
 
   def apply(self):
