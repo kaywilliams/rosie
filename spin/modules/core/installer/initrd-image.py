434e5aba8953c3bcbe434cf479f6239ff0c6747b
Kay Williams <kwilliams@renditionsoftware.com>
2008-01-09 16:54:12 -0800

    initrd-image/xen-images: events run when kickstart contents change
    The initrd-image and xen-images events were not running when the
    contents of the kickstart file changed. This was because the
    kickstart-file cvar was listed under 'variables' in the difftest data,
    not under 'input'. Fixed by this commit.

---
+++ spin/modules/core/installer/initrd-image.py
@@ -0,0 +1,53 @@
+from spin.event   import Event
+
+from spin.modules.shared import ImageModifyMixin
+
+API_VERSION = 5.0
+EVENTS = {'installer': ['InitrdImageEvent']}
+
+class InitrdImageEvent(Event, ImageModifyMixin):
+  def __init__(self):
+    Event.__init__(self,
+      id = 'initrd-image',
+      provides = ['isolinux-files'],
+      requires = ['anaconda-version', 'buildstamp-file'],
+      conditionally_requires = ['initrd-image-content', 'kickstart-file', 'ks-path'],
+      comes_after = ['isolinux'],
+    )
+
+    self.DATA = {
+      'config':    ['.'],
+      'variables': ['cvars[\'anaconda-version\']'],
+      'input':     [], # to be filled later
+      'output':    []  # to be filled later
+    }
+
+    ImageModifyMixin.__init__(self, 'initrd.img')
+
+  def error(self, e):
+    try:
+      self._close()
+    except:
+      pass
+    Event.error(self, e)
+
+  def setup(self):
+    self.diff.setup(self.DATA)
+    self.DATA['input'].append(self.cvars['kickstart-file'])
+    self.image_locals = self.locals.files['isolinux']['initrd.img']
+    ImageModifyMixin.setup(self)
+
+  def run(self):
+    self._modify()
+
+  def apply(self):
+    self.io.clean_eventcache()
+    self.cvars['isolinux-files']['initrd.img'] = self.SOFTWARE_STORE/self.image_locals['path']
+
+  def _generate(self):
+    ImageModifyMixin._generate(self)
+    self._write_buildstamp()
+
+    # copy kickstart file
+    if self.cvars['kickstart-file'] and self.cvars['ks-path']:
+      self.image.write(self.cvars['kickstart-file'], self.cvars['ks-path'].dirname)
