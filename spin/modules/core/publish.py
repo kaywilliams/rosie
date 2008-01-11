cbf1706a567e53b2b9b618b62b4b4adaf9b0a797
Kay Williams <kwilliams@renditionsoftware.com>
2008-01-11 14:47:37 -0800

    publish: workaround to get incremental sync working for publish
    Most events deal with output cleanup via the FileIO.clean_eventcache
    method. This method is not sufficient for the publish event, however,
    since it stores content outside of its metadata folder.  To deal with
    this, publish had been deleting all files in the publish folder
    prior to hard linking updated content. In typical machine
    configurations where the cache and the publish folder are on the
    same partition, this can be accomplished with minimal performance
    overhead.  When cache and publish folder are on separate partitions,
    however sync copies the files rather than linking them.
    
    The best fix for this issue would be to extend FileIO.clean_eventcache
    and the difftest output handler support cleanup of output files outside
    the metadata folder. This involves some fairly deep infrastructure work
    as well as changes to many events.
    
    This commit is a workaround that cobbles together strict syncing with
    some manual cleanup activities to accomplish similar yet incomplete
    results. The case that this workaround does not address is cleaning up
    extraneous file in the publish dir during sessions when the publish
    event does not run. This is not a huge issue, but it is an inconsistency
    between publish and other events.

---
+++ spin/modules/core/publish.py
@@ -0,0 +1,104 @@
+import fcntl
+import socket
+import struct
+
+from rendition import pps
+from rendition import shlib
+
+from spin.constants import *
+from spin.event     import Event
+from spin.logging   import L1
+
+P = pps.Path
+
+API_VERSION = 5.0
+EVENTS = {'ALL': ['PublishEvent'], 'setup': ['PublishSetupEvent']}
+
+class PublishSetupEvent(Event):
+  def __init__(self):
+    Event.__init__(self,
+      id = 'publish-setup',
+      provides = ['publish-content', 'publish-path', 'web-path'],
+    )
+
+    self.DATA = {
+      'variables': ['pva'],
+      'config': ['.'],
+    }
+
+  def setup(self):
+    self.diff.setup(self.DATA)
+
+    prefix = \
+      P(self.config.get('path-prefix/text()', 'distros')) / self.pva
+    web_path = \
+      self.config.get('remote-webroot/text()', None) or \
+        P('http://' +  self._get_host()) / prefix
+    self.web_path = P(web_path)
+    self.publish_path = \
+      P(self.config.get('local-webroot/text()', '/var/www/html')) / prefix
+
+  def apply(self):
+    self.cvars['publish-content'] = set()
+    self.cvars['publish-path'] = self.publish_path
+    self.cvars['web-path'] = self.web_path
+
+    self.diff.write_metadata()
+
+  def _get_host(self, ifname='eth0'):
+    if self.config.get('remote-webroot/@use-hostname', 'False') in BOOLEANS_TRUE:
+      return socket.gethostname()
+    else:
+      # TODO - improve this, it's not particularly accurate in some cases
+      s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+      return socket.inet_ntoa(fcntl.ioctl(s.fileno(),
+                                          0x8915,
+                                          struct.pack('256s', ifname[:15]))[20:24])
+
+
+class PublishEvent(Event):
+  def __init__(self):
+    Event.__init__(self,
+      id = 'publish',
+      requires = ['publish-path', 'publish-content'],
+    )
+
+    self.DATA =  {
+      'variables': ['cvars[\'publish-path\']',
+                    'cvars[\'publish-content\']'],
+      'input':     [],
+      'output':    [],
+    }
+
+  def setup(self):
+    self.diff.setup(self.DATA)
+    for dir in self.cvars['publish-content']:
+      self.io.setup_sync(self.cvars['publish-path'], paths=[dir])
+
+  # overriding Event method to remove publish-path which is outside the mddir
+  # this is a hack, better would be to generalize clean_eventcache
+  # to clean all event output, not just output in the metadata dir
+  def clean(self):
+    Event.clean(self)
+    self.cvars['publish-path'].rm(recursive=True, force=True)
+
+  def run(self):
+    "Publish the contents of SOFTWARE_STORE to PUBLISH_STORE"
+    self.log(1, L1("publishing to '%s'" % self.cvars['publish-path']))
+
+    # using link w/strict rather than io.sync to remove files outside the mddir
+    for path in self.cvars['publish-content']:
+      self.link(path, self.cvars['publish-path'], strict=True)
+
+    # clean-up obsolete/extraneous item at the root publish dir
+    for path in self.cvars['publish-path'].findpaths(mindepth=1, maxdepth=1):
+      if path.basename not in [x.basename for x in self.cvars['publish-content']]: 
+        path.rm(recursive=True, force=True)
+
+    shlib.execute('chcon -R --type=httpd_sys_content_t %s' \
+                    % self.cvars['publish-path'])
+
+    self.diff.write_metadata()
+
+  def apply(self):
+    self.io.clean_eventcache()
