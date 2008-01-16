54af0d7890d6be563fd6cec453413e8af32a3bfc
Uday Prakash <uprakash@renditionsoftware.com>
2008-01-16 14:00:45 -0800

    moved KERNELS constant from comps.py to constants.py

--- spin/modules/core/software/comps.py
+++ spin/modules/core/software/comps.py
@@ -3,15 +3,10 @@ from rendition import xmllib
 
 from spin.event     import Event
 from spin.logging   import L1
-from spin.constants import BOOLEANS_TRUE
+from spin.constants import BOOLEANS_TRUE, KERNELS
 
 API_VERSION = 5.0
 EVENTS = {'software': ['CompsEvent']}
-
-# list of all currently-known kernel package equivalents
-KERNELS = [ 'kernel', 'kernel-smp', 'kernel-zen', 'kernel-zen0',
-            'kernel-enterprise', 'kernel-hugemem', 'kernel-bigmem',
-            'kernel-BOOT' ]
 
 class CompsEvent(Event):
   def __init__(self):
