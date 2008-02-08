539a74b6dd92ad9b8f9cfeb3daa434519786da90
Uday Prakash <uprakash@renditionsoftware.com>
2008-02-07 16:19:55 -0800

    logos-rpm: bumped event version

--- spin/modules/core/rpms/logos-rpm.py
+++ spin/modules/core/rpms/logos-rpm.py
@@ -17,7 +17,7 @@ class LogosRpmEvent(RpmBuildMixin, Event, ImagesGenerator):
   def __init__(self):
     Event.__init__(self,
       id = 'logos-rpm',
-      version = '0.9',
+      version = '0.91',
       requires = ['base-info', 'anaconda-version', 'logos-versions'],
       provides = ['custom-rpms-data']
     )
