6435daaec2130f1fa62e9ec42f943a2d8738bb38
Kay Williams <kwilliams@renditionsoftware.com>
2008-03-26 17:24:24 -0700

    locals: updated buildstamp format for anaconda 11.4.0.99 (fedora 9 Beta)

--- spin/locals.py
+++ spin/locals.py
@@ -120,16 +120,20 @@ DISCINFO_FORMAT_LOCALS = LocalsDict({
 
 BUILDSTAMP_FORMAT_LOCALS = LocalsDict({
   '0': {
-    'timestamp': dict(index=0, string='%(timestamp)s'),
-    'fullname':  dict(index=1, string='%(fullname)s'),
-    'version':   dict(index=2, string='%(version)s'),
-    'product':   dict(index=3, string='%(product)s'),
+    'timestamp':   dict(index=0, string='%(timestamp)s'),
+    'fullname':    dict(index=1, string='%(fullname)s'),
+    'version':     dict(index=2, string='%(version)s'),
+    'productpath': dict(index=3, string='%(productpath)s'),
   },
   '10.2.0.63-1': {
-    'timestamp': dict(string='%(timestamp)s.%(basearch)s'),
+    'timestamp':   dict(string='%(timestamp)s.%(basearch)s'),
   },
   '10.2.1.5': {
-    'webloc':    dict(index=4, string='%(webloc)s'),
+    'webloc':      dict(index=4, string='%(webloc)s'),
+  },
+  '11.4.0.55': {
+    'productpath': REMOVE,
+    'webloc':      dict(index=3, string='%(webloc)s'),
   },
 })
 
