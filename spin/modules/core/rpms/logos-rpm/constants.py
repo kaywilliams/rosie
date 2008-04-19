2f6d354df16519230b592a58bb7d0dac0a05cbec
Uday Prakash <uprakash@renditionsoftware.com>
2008-04-18 19:22:33 -0700

    logos-rpm/constants: added color information

--- spin/modules/core/rpms/logos-rpm/constants.py
+++ spin/modules/core/rpms/logos-rpm/constants.py
@@ -13,46 +13,47 @@ DISTRO_INFO = {
   'CentOS': {
     '5': {
       'folder': 'c5',
-      'start_color': (),
-      'end_color': (),
+      'start_color': (33, 85, 147),
+      'end_color': (30, 81, 140),
     }
   },
   'Fedora Core': {
     '6': {
       'folder':'f6',
-      'start_color': (),
-      'end_color': (),
+      'start_color': (0, 37, 77),
+      'end_color': (0, 32, 68),
     }
   },
   'Fedora': {
     '7': {
       'folder': 'f7',
-      'start_color': (),
-      'end_color': (),
+      'start_color': (0, 27, 82),
+      'end_color': (28, 41, 89),
     },
     '8': {
       'folder': 'f8',
-      'start_color': (),
-      'end_color': ()
+      'start_color': (32, 75, 105),
+      'end_color': (70, 110, 146)
     },
     '9': {
+      ## UPDATEME
       'folder': 'f8',
-      'start_color': (),
-      'end_color': ()
+      'start_color': (32, 75, 105),
+      'end_color': (70, 110, 146)
     },
   },
   'Red Hat Enterprise Linux Server': {
     '5': {
       'folder': 'r5',
-      'start_color': (),
-      'end_color': ()
+      'start_color': (120, 30, 29),
+      'end_color': (88, 23, 21)
     }
   },
   '*': { # default
     '0': {
       'folder': 'r5',
-      'start_color': (),
-      'end_color': ()
+      'start_color': (120, 30, 29),
+      'end_color': (88, 23, 21)
     }
   }
 }
