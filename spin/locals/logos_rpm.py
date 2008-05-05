457b4537ef27c3927e316cd8660e290763a8fdaa
Uday Prakash <uprakash@renditionsoftware.com>
2008-05-05 16:14:34 -0700

    locals/logos_rpm: added distro-specific locals dictionary
    The L_LOGOS_RPM_INFO locals dictionary contains distro-specific
    information: the scriptlets to be installed for that distro, the
    shared folder to look in for the files to be installed by the RPM, the
    colors to use for the images we create etc.
    
    Now that this information is in a locals dictionary, we can get rid of
    all the conditional logic in the logos RPM event.

--- spin/locals/logos_rpm.py
+++ spin/locals/logos_rpm.py
@@ -1,6 +1,6 @@
 from spin.locals import LocalsDict, REMOVE
 
-__all__ = ['L_LOGOS_RPM_FILES']
+__all__ = ['L_LOGOS_RPM_FILES', 'L_LOGOS_RPM_INFO']
 
 L_LOGOS_RPM_FILES = LocalsDict({
   "anaconda-0": {
@@ -382,3 +382,160 @@ L_LOGOS_RPM_FILES = LocalsDict({
     ),
   },
 })
+
+DEFAULT_INFO = {
+  'folder': 'r5',
+  'start_color': (120, 30, 29),
+  'end_color': (88, 23, 21),
+  'triggerin': {
+    'gdm': '''CUSTOM_CONF=%%{_sysconfdir}/gdm/custom.conf
+THEME_CONF=/usr/share/%(rpm_name)s/custom.conf
+%%{__mv} -f $CUSTOM_CONF $CUSTOM_CONF.rpmsave
+%%{__cp} $THEME_CONF $CUSTOM_CONF
+''',
+    'desktop-backgrounds-basic': '''BACKGROUNDS=/usr/share/backgrounds
+DEFAULTS="default-5_4.jpg default-dual.jpg default-dual-wide.jpg default.jpg default-wide.jpg"
+for default in $DEFAULTS; do
+  file=$BACKGROUNDS/images/$default
+  if [ -e $file ]; then
+    %%{__mv} $file $file.rpmsave
+    %%{__ln_s} $BACKGROUNDS/spin/$default $file
+  fi
+done
+''',
+  },
+  'triggerun': {
+    'gdm': '''CUSTOM_CONF=%%{_sysconfdir}/gdm/custom.conf
+if [ "$2" -eq "0" -o "$1" -eq "0" ]; then
+  %%{__rm} -f $CUSTOM_CONF.rpmsave
+fi
+''',
+    'desktop-backgrounds-basic': '''BACKGROUNDS=/usr/share/backgrounds
+if [ "$2" -eq "0" -o "$1" -eq "0" ]; then
+  for default in `ls -1 $BACKGROUNDS/images/default* | grep -v "rpmsave"`; do
+    %%{__rm} -f $default
+    %%{__mv} -f $default.rpmsave $default
+  done
+fi
+''',
+  },
+}
+
+L_LOGOS_RPM_INFO = {
+  'Fedora': LocalsDict({
+    '0': DEFAULT_INFO,
+    '7': {
+      'folder': 'f7',
+      'start_color': (0, 27, 82),
+      'end_color': (28, 41, 89),
+    },
+    '8': {
+      'folder': 'f8',
+      'start_color': (32, 75, 105),
+      'end_color': (70, 110, 146),
+      'post-install': '''SPIN_BACKGROUNDS="1-spin-sunrise.png 2-spin-day.png 3-spin-sunset.png 4-spin-night.png"
+DEFAULT=/usr/share/backgrounds/spin/default.jpg
+for file in $SPIN_BACKGROUNDS; do
+  %{__ln_s} $DEFAULT /usr/share/backgrounds/spin/$file
+done
+''',
+      'post-uninstall': '''SPIN_BACKGROUNDS="1-spin-sunrise.png 2-spin-day.png 3-spin-sunset.png 4-spin-night.png"
+for file in $SPIN_BACKGROUNDS; do
+  %{__rm} -f /usr/share/backgrounds/spin/$file
+done
+''',
+      'triggerin': {
+        'desktop-backgrounds-basic': '''BACKGROUNDS=/usr/share/backgrounds
+DEFAULTS="default-5_4.jpg default-dual.jpg default-dual-wide.jpg default.jpg default-wide.jpg"
+for default in $DEFAULTS; do
+  file=$BACKGROUNDS/images/$default
+  if [ -e $file ]; then
+    %%{__mv} $file $file.rpmsave
+    %%{__ln_s} $BACKGROUNDS/spin/$default $file
+  fi
+done
+if [ -e $BACKGROUNDS/infinity ]; then
+  %%{__mv} $BACKGROUNDS/infinity $BACKGROUNDS/infinity.rpmsave,
+  %%{__ln_s} $BACKGROUNDS/spin $BACKGROUNDS/infinity
+  if [ ! -e $BACKGROUNDS/spin/infinity.xml ]; then
+    %%{__ln_s} $BACKGROUNDS/spin/spin.xml $BACKGROUNDS/spin/infinity.xml
+  fi
+fi
+'''
+      },
+      'triggerun': {
+        'desktop-backgrounds-basic': '''BACKGROUNDS=/usr/share/backgrounds
+if [ "$2" -eq "0" -o "$1" -eq "0" ]; then
+  for default in `ls -1 $BACKGROUNDS/images/default* | grep -v "rpmsave"`; do
+    %%{__rm} -f $default
+    %%{__mv} -f $default.rpmsave $default
+  done
+  %%{__rm} -rf $BACKGROUNDS/infinity
+  %%{__mv} -f $BACKGROUNDS/infinity.rpmsave $BACKGROUNDS/infinity
+  %%{__rm} -f $BACKGROUNDS/spin/infinity.xml
+fi
+''',
+      },
+    },
+    '9': {
+      ## UPDATEME
+      'folder': 'f8',
+      'start_color': (32, 75, 105),
+      'end_color': (70, 110, 146),
+      'triggerin': {
+        'desktop-backgrounds-basic': '''BACKGROUNDS=/usr/share/backgrounds
+DEFAULTS="default-5_4.jpg default-dual.jpg default-dual-wide.jpg default.jpg default-wide.jpg"
+for default in $DEFAULTS; do
+  file=$BACKGROUNDS/images/$default
+  if [ -e $file ]; then
+    %%{__mv} $file $file.rpmsave
+    %%{__ln_s} $BACKGROUNDS/spin/$default $file
+  fi
+done
+if [ -e $BACKGROUNDS/waves ]; then
+  %%{__mv} $BACKGROUNDS/waves $BACKGROUNDS/waves.rpmsave,
+  %%{__ln_s} $BACKGROUNDS/spin $BACKGROUNDS/waves
+  if [ ! -e $BACKGROUNDS/spin/waves.xml ]; then
+    %%{__ln_s} $BACKGROUNDS/spin/spin.xml $BACKGROUNDS/spin/waves.xml
+  fi
+fi
+'''
+      },
+      'triggerun': {
+        'desktop-backgrounds-basic': '''BACKGROUNDS=/usr/share/backgrounds
+if [ "$2" -eq "0" -o "$1" -eq "0" ]; then
+  for default in `ls -1 $BACKGROUNDS/images/default* | grep -v "rpmsave"`; do
+    %%{__rm} -f $default
+    %%{__mv} -f $default.rpmsave $default
+  done
+  %%{__rm} -rf $BACKGROUNDS/waves
+  %%{__mv} -f $BACKGROUNDS/waves.rpmsave $BACKGROUNDS/waves
+  %%{__rm} -f $BACKGROUNDS/spin/waves.xml
+fi
+''',
+      },
+    },
+  }),
+  'CentOS': LocalsDict({
+    '0': DEFAULT_INFO,
+    '5': {
+      'folder': 'c5',
+      'start_color': (33, 85, 147),
+      'end_color': (30, 81, 140),
+    },
+  }),
+  'Fedora Core': LocalsDict({
+    '0': DEFAULT_INFO,
+    '6': {
+      'folder': 'f6',
+      'start_color': (0, 37, 77),
+      'end_color': (0, 32, 68),
+    },
+  }),
+  'Red Hat Enterprise Linux Server': LocalsDict({
+    '0': DEFAULT_INFO,
+  }),
+  '*': LocalsDict({
+    '0': DEFAULT_INFO,
+  }),
+}
