e0385f900d11558708ce2bb4dc90818065588e38
Uday Prakash <uprakash@renditionsoftware.com>
2008-09-30 16:33:36 -0700

    Added new installclass file for anaconda >= 11.4.1.10-1.
    The getBackend() method doesn't accept any parameters anymore.

--- spin/locals/installclass.py
+++ spin/locals/installclass.py
@@ -113,4 +113,63 @@ class InstallClass(BaseInstallClass):
     BaseInstallClass.__init__(self, expert)
 '''
 ,
+  "anaconda-11.4.1.10-1":
+'''
+from installclass import BaseInstallClass
+from constants import *
+from filer import *
+from flags import flags
+import os, types
+import iutil
+
+import gettext
+_ = lambda x: gettext.ldgettext("anaconda", x)
+
+import installmethod
+import yuminstall
+
+import rpmUtils.arch
+
+class InstallClass(BaseInstallClass):
+  id = "custom"
+  _name = N_("_Custom")
+  pixmap = "custom.png"
+  _description = N_("Select the software you would like to install on your system.")
+  sortPriority = 10000
+  showLoginChoice = 1
+  showMinimal = 1
+
+  tasks = [("Default", %(default_groups)s), ("Everything", %(all_groups)s)]
+
+  sortPriority = 10000
+
+  bugFiler = BugzillaFiler(bugUrl="https://bugzilla.redhat.com/xmlrpc.cgi")
+
+  def getPackagePaths(self, uri):
+    if not type(uri) == types.ListType:
+      uri = [uri,]
+
+    return {'Installation Repo': uri}
+
+  def setInstallData(self, anaconda):
+    BaseInstallClass.setInstallData(self, anaconda)
+
+    if not anaconda.isKickstart:
+      BaseInstallClass.setDefaultPartitioning(self, anaconda.id.partitions,
+                                              CLEARPART_TYPE_LINUX)
+
+  def setSteps(self, anaconda):
+    BaseInstallClass.setSteps(self, anaconda);
+    anaconda.dispatch.skipStep("partition")
+
+  def getBackend(self):
+    if flags.livecdInstall:
+      import livecd
+      return livecd.LiveCDCopyBackend
+    else:
+      return yuminstall.YumBackend
+
+  def __init__(self):
+    BaseInstallClass.__init__(self)
+''',
 })
