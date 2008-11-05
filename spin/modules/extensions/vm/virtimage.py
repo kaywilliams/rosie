dcfe03fc1cb7f52562ea6aba4cf3a16e48f4ad6e
Daniel Musgrave <dmusgrave@renditionsoftware.com>
2008-11-05 10:13:09 -0800

    updated virtimage to work incrementally spin/modules/extensions/vm/virtimage.py  * no longer tracks 'pkglist', instead tracks 'pkglist-install-packages' cvar  * virtimage now attempts to use prior output when dealing with images; if    the partition layout has not changed between runs, prior output is used;    otherwise, we start over  * now defines two fs subclasses, SpinExtDiskMount and SpinPartitionedMount,    which extend classes in imgcreate and appcreate, respectively  * these new classes allow passing of a 'format' parameter to various methods    that controls whether or not partitioning/formatting commands actually    run; by passing false, existing partitions are used instead of creating    new ones.  * the above is a major hack that is by no means a 'realistic' solution; it    merely serves the purpose of enabling incremental builds for virtimage,    nothing more

--- spin/modules/extensions/vm/virtimage.py
+++ spin/modules/extensions/vm/virtimage.py
@@ -50,13 +50,12 @@ class LibvirtVMEvent(vms.VmCreateMixin, Event):
 
     self.builddir = self.mddir / 'build'
     self.tmpdir   = self.mddir / 'tmp'
-    self.app      = self.mddir / '%s.img' % self.applianceid
 
     self.DATA =  {
       'config': ['.'],
       'input':  [],
       'output': [],
-      'variables': ['cvars[\'pkglist\']'],
+      'variables': ['cvars[\'pkglist-install-packages\']'],
     }
 
     self.creator = None
@@ -93,9 +92,24 @@ class LibvirtVMEvent(vms.VmCreateMixin, Event):
 
   def run(self):
 
+    # if scripts or partitions change, remove base
+    if ( not self._check_ks_scripts() or
+         not self._check_partitions() ):
+      self.mddir.listdir('*.raw').rm(force=True)
+
+    # prepare base_on
+    base_on = {}
+    for part in self.ks.handler.partition.partitions:
+      if not (self.mddir/'%s-%s.raw' % (self.applianceid, part.disk)).exists():
+        # if any disk doesn't exist for a partition, give up and start over
+        self.mddir.listdir('*.raw').rm(force=True)
+        base_on = None
+        break
+      else:
+        base_on[self.mddir/'%s-%s.raw' % (self.applianceid, part.disk)] = part.disk
+
     try:
-      ##self.creator.mount(base_on=X, cachedir=self.buiddir)
-      self.creator.mount(cachedir=self.builddir)
+      self.creator.mount(base_on=base_on, cachedir=self.builddir)
       self.creator.install()
       self.creator.configure()
       self.creator.unmount()
@@ -107,6 +121,7 @@ class LibvirtVMEvent(vms.VmCreateMixin, Event):
     self.io.clean_eventcache()
 
     self.cvars.setdefault('publish-content', set())
+
     for part in self.ks.handler.partition.partitions:
       self.cvars['publish-content'].add(self.mddir/'%s-%s.raw'
                                          % (self.applianceid, part.disk))
@@ -123,12 +138,190 @@ class SpinApplianceImageCreator(vms.SpinImageCreatorMixin,
     if 'grub' not in self._get_pkglist_names():
       raise GrubRequiredError()
 
+  def _mount_instroot(self, base_on = None):
+
+    # base_on should be a pps filename: diskname pair
+    if base_on is not None:
+      # TODO - this could be refactored with existing _mount_instroot, prolly
+      self._setattr_('__imgdir', self._mkdtemp())
+
+      # create disks
+      for id, name in base_on.items():
+        # move old base(s) into place
+        if id.exists():
+          id.cp(self._getattr_('__imgdir'), link=True, force=True)
+
+        disk = appcreate.SparseLoopbackDisk(
+                '%s/%s' % (self._getattr_('__imgdir'), id.basename),
+                id.stat().st_size)
+        self._getattr_('__disks')[name] = disk
+
+      self._setattr_('__instloop', SpinPartitionedMount( #!
+                                     self._getattr_('__disks'),
+                                     self._instroot))
+
+      for part in self.ks.handler.partition.partitions:
+        self._getattr_('__instloop').add_partition(
+          int(part.size), part.disk, part.mountpoint, part.fstype)
+
+      try:
+        self._getattr_('__instloop').mount(format=False)
+      except appcreate.MountError, e:
+        raise imgcreate.CreatorError("Failed mount disks: %s" % e)
+
+      vms.SpinImageCreatorMixin._base_on(self, base_on)
+
+    else:
+      appcreate.ApplianceImageCreator._mount_instroot(self)
+
   def _cleanup(self):
-    pass
-    ##if self._instroot and pps.path(self._instroot).exists():
-    ##  self.event.vmdir.rm(recursive=True, force=True)
-    ##  pps.path(self._instroot).rename(self.event.vmdir)
+    try:
+      for file in pps.path(self._getattr_('__imgdir')).listdir():
+        file.cp(self.mddir, link=True, force=True)
+    except:
+      pass
 
 class GrubRequiredError(SpinError):
   message = ( "Creating an appliance virtual image requires that the 'grub' "
               "package be included in the appliance." )
+
+import inspect
+import logging
+import subprocess
+
+# the following classes use the same 'Xtreem Kool Hak' as
+# SpinImageCreatorMixin for _getattr_ (see shared/vms.py for details)
+#
+# These classes allow appcreator to use base_on for its images - specifically,
+# the disk images are not automatically formatted upon creation
+#
+# Most of the method code here is unchanged from the original; lines with
+# differences end in '#!'
+
+class SpinExtDiskMount(imgcreate.ExtDiskMount):
+  def _getattr_(self, attr):
+    for cls in inspect.getmro(self.__class__):
+      if cls == SpinExtDiskMount: continue # don't loop infinitely
+      atn = '_%s%s' % (cls.__name__, attr)
+      if hasattr(self, atn):
+        return getattr(self, atn)
+    else:
+      raise AttributeError(attr)
+
+  def __create(self, format=True):
+    # only formats if format is True, otherwise passes
+    resize = False
+    if not self.disk.fixed() and self.disk.exists():
+      resize = True
+
+    self.disk.create()
+
+    if resize:
+      self._getattr_('__resize_filesystem')()
+    else:
+      if format: self._getattr_('__format_filesystem')() #!
+
+  def mount(self, format=True):
+    self.__create(format=format) #!
+    imgcreate.DiskMount.mount(self)
+
+class SpinPartitionedMount(appcreate.PartitionedMount):
+  def _getattr_(self, attr):
+    for cls in inspect.getmro(self.__class__):
+      if cls == SpinPartitionedMount: continue # don't loop infinitely
+      atn = '_%s%s' % (cls.__name__, attr)
+      if hasattr(self, atn):
+        return getattr(self, atn)
+    else:
+      raise AttributeError(attr)
+
+  def __format_disks(self, format=True):
+    # only formats if format is True, otherwise identical
+    if format: #!
+      logging.debug("Formatting disks")
+      for dev in self.disks.keys():
+        d = self.disks[dev]
+        logging.debug("Initializing partition table for %s" % (d['disk'].device))
+        rc = subprocess.call(["/sbin/parted", "-s", d['disk'].device, "mklabel", "msdos"]) 
+        if rc != 0:
+          raise appcreate.MountError("Error writing partition table on %s" % d['disk'].device)
+
+    logging.debug("Assigning partitions to disks")
+    for n in range(len(self.partitions)):
+      p = self.partitions[n]
+
+      if not self.disks.has_key(p['disk']):
+        raise appcreate.MountError("No disk %s for partition %s" % (p['disk'], p['mountpoint']))
+
+      d = self.disks[p['disk']]
+      d['numpart'] += 1
+      if d['numpart'] > 3:
+        # Increase allocation of extended partition to hold this partition
+        d['extended'] += p['size']
+        p['type'] = 'logical'
+        p['num'] = d['numpart'] + 1
+      else:
+        p['type'] = 'primary'
+        p['num'] = d['numpart']
+
+      p['start'] = d['offset']
+      d['offset'] += p['size']
+      d['partitions'].append(n)
+      logging.debug("Assigned %s to %s%d at %d at size %d" % (p['mountpoint'], p['disk'], p['num'], p['start'], p['size']))
+
+    # XXX we should probably work in cylinder units to keep fdisk happier..
+    if format: #!
+      start = 0
+      logging.debug("Creating partitions")
+      for p in self.partitions:
+        d = self.disks[p['disk']]
+        if p['num'] == 5:
+          logging.debug("Added extended part at %d of size %d" % (p['start'], d['extended']))
+          rc = subprocess.call(["/sbin/parted", "-s", d['disk'].device, "mkpart", "extended",
+                                "%dM" % p['start'], "%dM" % (p['start'] + d['extended'])])
+
+        logging.debug("Add %s part at %d of size %d" % (p['type'], p['start'], p['size']))
+        rc = subprocess.call(["/sbin/parted", "-s", d['disk'].device, "mkpart",
+                              p['type'], "%dM" % p['start'], "%dM" % (p['start']+p['size'])])
+
+        # XXX disabled return code check because parted always fails to
+        # reload part table with loop devices. Annoying because we can't
+        # distinguish this failure from real partition failures :-(
+        if rc != 0 and 1 == 0:
+          raise appcreate.MountError("Error creating partition on %s" % d['disk'].device)
+
+  def mount(self, format=True):
+    # only formats if format is True; creates custom classes of mount/disks
+    # otherwise identical
+    for dev in self.disks.keys():
+      d = self.disks[dev]
+      d['disk'].create()
+
+    self.__format_disks(format=format) #!
+    self._getattr_('__map_partitions')()
+    self._getattr_('__calculate_mountorder')()
+
+    for mp in self.mountOrder:
+      p = None
+      for p1 in self.partitions:
+        if p1['mountpoint'] == mp:
+          p = p1
+          break
+
+      if mp == 'swap':
+        subprocess.call(["/sbin/mkswap", p['device']])
+        continue
+
+      rmmountdir = False
+      if p['mountpoint'] == "/":
+        rmmountdir = True
+      pdisk = SpinExtDiskMount( #!
+                imgcreate.RawDisk(p['size'] * 1024 * 1024, p['device']),
+                self.mountdir + p['mountpoint'],
+                p['fstype'],
+                4096,
+                p['mountpoint'],
+                rmmountdir)
+      pdisk.mount(format=format) #!
+      p['mount'] = pdisk
+
