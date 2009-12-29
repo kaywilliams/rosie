a95310d9d2d409389f69fe83d29977e2a4643a5a
Kay Williams <kwilliams@renditionsoftware.com>
2009-12-28 19:15:16 -0800

    reverted kickstart module, test module, and DDFR documentation to revision 1661 given pykickstart limitation in r5/c5

--- sbtest/modules/kickstart.py
+++ sbtest/modules/kickstart.py
@@ -22,10 +22,17 @@ from sbtest.mixins import touch_input_files, remove_input_files
 class KickstartEventTestCase(EventTestCase):
   moduleid = 'kickstart'
   eventid  = 'kickstart'
-  _conf = [
-    "<kickstart/>",
-    "<rpmbuild enabled='false'/>",
-  ]
+  _conf = """<kickstart>infile</kickstart>"""
+
+  def setUp(self):
+    EventTestCase.setUp(self)
+    if self.event:
+      touch_input_files(self.event._config.file.abspath().dirname)
+
+  def tearDown(self):
+    if self.event:
+      remove_input_files(self.event._config.file.abspath().dirname)
+    EventTestCase.tearDown(self)
 
 def make_suite(distro, version, arch):
   suite = ModuleTestSuite('kickstart')
