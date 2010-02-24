1271850b7a652e2ed3a1b427ca1516f5454d71bf
Kay Williams <kwilliams@renditionsoftware.com>
2010-02-24 08:40:58 -0800

    bumped config module version to include new yum-sync plugin

--- systembuilder/modules/core/rpmbuild/config.py
+++ systembuilder/modules/core/rpmbuild/config.py
@@ -38,7 +38,7 @@ class ConfigEvent(RpmBuildMixin, Event):
     Event.__init__(self,
       id = 'config',
       parentid = 'rpmbuild',
-      version = '0.99',
+      version = '1.01',
       provides = ['rpmbuild-data'],
       requires = ['input-repos'],
       conditionally_requires = ['web-path', 'gpgsign-public-key'],
