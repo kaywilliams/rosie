6f166032791f3627b7907d96cba1686b352215f6
Kay Williams <kwilliams@renditionsoftware.com>
2011-06-06 16:59:18 -0700

    don't try to validate kickstart content the installed pykickstart doesn't know about

--- systemstudio/locals/pykickstart.py
+++ systemstudio/locals/pykickstart.py
@@ -5,13 +5,15 @@ __all__ = ['L_PYKICKSTART']
 L_PYKICKSTART = LocalsDict({
   "pykickstart-0": """
 from pykickstart.parser import *
-try:
-  ksdata = KickstartData()
-  kshandlers = KickstartHandlers(ksdata)
-  parser = KickstartParser(ksdata,kshandlers)
-  parser.readKickstart('%(ksfile)s')
-except (KickstartParseError, KickstartValueError), e:
-  raise KickstartValidationError(e.value + '\\n' + self.kssource)""",
+# el5 pykickstart doesn't understand el6 kickstart syntax, e.g. end
+if self.cvars['base-info']['version'] <= '6':
+  try:
+    ksdata = KickstartData()
+    kshandlers = KickstartHandlers(ksdata)
+    parser = KickstartParser(ksdata,kshandlers)
+    parser.readKickstart('%(ksfile)s')
+  except (KickstartParseError, KickstartValueError), e:
+    raise KickstartValidationError(e.value + '\\n' + self.kssource)""",
   "pykickstart-1.74": """
 from pykickstart.parser  import *
 from pykickstart.version import makeVersion
