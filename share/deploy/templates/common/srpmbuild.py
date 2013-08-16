f09ac7d280187d45ed514d395c8f5710279b116b
Kay Williams <kay@deployproject.org>
2013-08-16 14:46:43 -0700

    template updates based on additional testing

--- share/deploy/templates/common/srpmbuild.py
+++ share/deploy/templates/common/srpmbuild.py
@@ -18,7 +18,7 @@ class Fetcher:
 
     def __init__(self, id, input, patches=None,
                  username=None, password=None,
-                 *args, **kwargs):
+                 last=None, *args, **kwargs):
         self.id = id
         self._input = pps.path(input).normpath()
 
@@ -43,11 +43,6 @@ class Fetcher:
     def initialize(self):
         pass
 
-    def check_status(self):
-        "uses current and last srpm name, version and release to determine"
-        "whether a new srpm should be built"
-        raise NotImplementedError()
-
     def build_srpm(self):
         "returns the path to the built srpm"
         raise NotImplementedError()
@@ -63,7 +58,7 @@ class LocalFetcher(Fetcher):
     type = 'local'
     arguments = ['makefile']
 
-    def __init__(self, id, input, last, patches=None,
+    def __init__(self, id, input, patches=None,
                  username=None, password=None,
                  makefile=None,
                  *args, **kwargs):
@@ -116,35 +111,25 @@ class LocalFetcher(Fetcher):
                 raise RuntimeError("No spec file '%s.spec' found" % self.id)
         return file
 
-    def check_status(self):
-      "compares current nvr to last _nvr; returns false if they differ"
-      if self.last:
-        # get last nvr
-        dist = shlib.execute('rpm --eval "%{dist}"')[0]
-        name, ver, rel, _, _ = miscutils.splitFilename(self.last)
-        last_nvr = [name, ver, rel.replace(dist, '')]
-
-        # get curr nvr
-        curr = self.nvra
-        curr_nvr = [ curr['name'], curr['version'], curr['release'] ]
-
-        # compare
-        if last_nvr == curr_nvr: return False
-        
-      return True
-
     def build_srpm(self):
-        for dir in ('BUILD', 'SRPMS', 'SPECS', 'SOURCES', 'RPMS'):
-            (self.builddir / dir).mkdirs()
-        self.make_srpm()
-        name = self.nvra['name']
-        ver  = self.nvra['version']
-        srpm = (self.builddir / 'SRPMS').findpaths(
-            glob='%s-%s-*.src.rpm' % (name, ver), maxdepth=1)[0]
-        #self.apply_patches()
+        srpm = self.make_srpm()
+
+        # if the name of the current srpm is the same as the last srpm,
+        # but the content differs, bump the spec release and rebuild
+        if (self.last and
+            self.last.basename == srpm.basename and      
+            shlib.execute('/usr/bin/rpmdev-diff -q %s %s'
+                         % (self.last_archive(), self.curr_archive(srpm)))):
+          self.bump_spec()
+          self._nvra = None # force spec to be reread
+          srpm = self.make_srpm()
         return srpm
 
     def make_srpm(self):
+        for dir in ('BUILD', 'SRPMS', 'SPECS', 'SOURCES', 'RPMS'):
+            (self.builddir / dir).rm(recursive=True, force=True)
+            (self.builddir / dir).mkdirs()
+
         cwd = os.getcwd()
         os.chdir(str(self.local_sources))
         if self.makefile is not None:
@@ -156,16 +141,40 @@ class LocalFetcher(Fetcher):
         shlib.execute(args, verbose=self.QUIET_MODE)
         os.chdir(cwd)
 
+        name = self.nvra['name']
+        ver  = self.nvra['version']
+        srpm = (self.builddir / 'SRPMS').findpaths(
+            glob='%s-%s-*.src.rpm' % (name, ver), maxdepth=1)[0]
+
+        return srpm
+
+    def bump_spec(self):
+      shlib.execute("/usr/bin/rpmdev-bumpspec -u 'Deploy Automated Package Builder' '%s'" % self.spec_file)
+
+    def curr_archive(self, curr):
+        dir = self.builddir / 'archives' / 'curr'
+        dir.mkdirs()
+        return  dir / shlib.execute(
+                      '/usr/bin/rpmdev-extract -C %s %s | grep -v "\.spec"'
+                      % (dir, curr))[0]
+
+    def last_archive(self):
+        dir = self.builddir / 'archives' / 'last'
+        dir.mkdirs()
+        return dir / shlib.execute(
+                     '/usr/bin/rpmdev-extract -C %s %s | grep -v "\.spec"'
+                     % (dir, self.last))[0]
+
     def _get_local_sources(self):
         raise NotImplementedError()
 
 
 class FolderFetcher(LocalFetcher):
     type = 'folder'
-    def __init__(self, id, input, last, patches=None,
+    def __init__(self, id, input, patches=None,
                  username=None, password=None,
                  *args, **kwargs):
-        LocalFetcher.__init__(self, id, input, last, patches=patches,
+        LocalFetcher.__init__(self, id, input, patches=patches,
                               username=username, password=password,
                               *args, **kwargs)
 
@@ -181,10 +190,10 @@ class FolderFetcher(LocalFetcher):
 
 class ScmFetcher(LocalFetcher):
     type = 'scm'
-    def __init__(self, id, input, last, patches=None,
+    def __init__(self, id, input, patches=None,
                  username=None, password=None,
                  *args, **kwargs):
-        LocalFetcher.__init__(self, id, input, last, patches=patches,
+        LocalFetcher.__init__(self, id, input, patches=patches,
                               username=username, password=password, *args, 
                               **kwargs)
 
@@ -193,10 +202,10 @@ class MercurialFetcher(ScmFetcher):
     type = 'mercurial'
     arguments = ['makefile', 'revision']
 
-    def __init__(self, id, input, last, patches=None, username=None, 
+    def __init__(self, id, input, patches=None, username=None, 
                  password=None, revision=None,
                  *args, **kwargs):
-        ScmFetcher.__init__(self, id, input, last, patches=patches,
+        ScmFetcher.__init__(self, id, input, patches=patches,
                             username=username, password=password,
                             *args, **kwargs)
 
@@ -227,25 +236,27 @@ class MercurialFetcher(ScmFetcher):
 CLASS_MAP = { 'folder': FolderFetcher,
               'mercurial': MercurialFetcher }
 
-def main(id, input, output, last, username, password, type, *args, **kwargs):
+def main(id, input, output, last, username, password, type,
+         *args, **kwargs):
   cls = CLASS_MAP[type]
 
-  fetcher = cls(id, input=input, last=last, 
-                             username=username, password=password, 
-                             revision=None, *args, **kwargs)
+  fetcher = cls(id, input=input, last=last,  username=username,
+                    password=password, revision=None, *args, **kwargs)
   fetcher.set_defaults(quiet_mode=False)
-  if fetcher.check_status():
-    try:
+
+  try:
+    srpm = fetcher.build_srpm()
+    if not last or not srpm.basename == last.basename:
       clean_output(output)
-      srpm = fetcher.build_srpm()
       srpm.cp(output)
-    except Exception:
-      message = ("An error occurred building the '%s' srpm:\n'%s'\n"
-                 "A copy of the srpm build environment is at '%s'. "
-                 "This folder will be removed automatically at "
-                 "the start of the next '%s' build."
-                 % (id, traceback.format_exc(), fetcher.builddir, id))
-      raise RuntimeError(message)
+  except Exception:
+    message = ("An error occurred building the '%s' srpm:\n'%s'\n"
+               "A copy of the srpm build environment is at '%s'. "
+               "This folder will be removed automatically at "
+               "the start of the next '%s' build."
+               % (id, traceback.format_exc(), fetcher.builddir, id))
+    raise RuntimeError(message)
+
   fetcher.builddir.rm(force=True, recursive=True)    
   sys.exit()
 
@@ -256,9 +267,9 @@ def clean_output(output):
 
 if __name__ == '__main__':
   srpmid = sys.argv[1]
-  input = pps.path(sys.argv[2]) # e.g. url to mercurial repository
+  input = pps.path(sys.argv[2]) # e.g. url to source content 
   output = pps.path(sys.argv[3]) # location to copy final srpm
-  last = sys.argv[4] # filename of last srpm
+  last = pps.path(sys.argv[4]) # path to last srpm
   type = sys.argv[5] # 'folder' or 'mercurial'
 
   # TODO - add input validation and useful errors
