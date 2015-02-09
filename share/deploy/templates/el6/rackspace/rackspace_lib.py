151b23652cf602a799046117f1b3c37166740131
Kay Williams <kay@deployproject.org>
2015-02-09 23:40:01 +0000

    rackspace: move delete functions into rackspace_lib for use by test-exists

--- share/deploy/templates/el6/rackspace/rackspace_lib.py
+++ share/deploy/templates/el6/rackspace/rackspace_lib.py
@@ -10,9 +10,6 @@ import novaclient
 import novaclient.auth_plugin
 import novaclient.v1_1.client as nova_client
 
-SSH_OPTS = ("-o BatchMode=yes "
-            "-o UserKnownHostsFile=%{ssh-host-key-file}")
-
 # read authfile
 d={}
 with open("/root/rackspace/rackspace_admin", 'r') as authfile:
@@ -69,9 +66,20 @@ def create_volume(name, size, type):
   volumes = nova_volume.volumes.findall(display_name=name)
 
   if len(volumes) == 0: # create volume
-    volume = nova_volume.volumes.create(size=size, 
+    volume = nova_volume.volumes.create(size=int(size), 
                                         display_name = name,
                                         volume_type=type)
+
+    # poll until volume is created 
+    seconds = 0
+    while True:
+      volume = nova_volume.volumes.get(volume.id)
+      if volume.status == 'available':
+        break
+      else:
+        sys.stdout.write("creating volume '%s'... %s seconds" % (name, seconds))
+        seconds += 2
+        time.sleep(2)
 
   elif len(volumes) == 1: # use existing volume
     volume = volumes[0]
@@ -95,15 +103,19 @@ def attach_volume(server_id, volume_id, device, name, fqdn):
     attach = True
 
   if attach:
-    nova.volumes.create_server_volume(server_id, volume_id, device)
+    try:
+      nova.volumes.create_server_volume(server_id, volume_id, device)
+    except novaclient.exceptions.BadRequest, e:
+      sys.stderr.write("Unable to attach volume '%s':\n\n%s" %s (name, e))
+      sys.exit(1)
 
     # poll until attachment complete
     seconds = 0
     while True:
       status =  nova_volume.volumes.get(volume_id).status
       if status == "attaching": 
-        print ("attaching '%s' volume at '%s'... %s seconds" % 
-              (name, device, seconds))
+        sys.stdout.write("attaching '%s' volume at '%s'... %s seconds" % 
+                        (name, device, seconds))
         seconds += 2
         time.sleep(2)
       elif status == "in-use" and \
@@ -114,6 +126,94 @@ def attach_volume(server_id, volume_id, device, name, fqdn):
         sys.stderr.write("The volume '%s' did not attach to '%s'. The "
                          "current status is '%s'.\n" % (name, fqdn, status))
         sys.exit(1)
+
+def delete_server(server, delete_volumes, ssh_host_key_file, ssh_host):
+  # get server volumes
+  server_volumes = nova.volumes.get_server_volumes(server.id)
+  
+  # unmount volumes
+  unmount_volumes(server, server_volumes, ssh_host_key_file, ssh_host)
+  
+  # delete server
+  _delete_server(server)
+  
+  # delete volumes, but only in the test-install case
+  if delete_volumes:
+    for v in server_volumes:
+      detach_volume(server.id, v)
+      delete_volume(v)
+
+def _delete_server(server):
+  def log_deleting(seconds, task_state, status):
+      sys.stdout.write("deleting... %s seconds (task state: %s, status: %s)\n"
+                       % (seconds, task_state, status))
+      seconds += 5 
+      time.sleep(5)
+
+  server.delete()
+  
+  # poll until server deleted
+  seconds = 0
+  while True:
+    try:
+      server = nova.servers.get(server.id)
+      task_state = getattr(server, 'OS-EXT-STS:task_state')
+
+      if str(server.status) == "ERROR":
+        if hasattr(server, 'fault') and 'message' in server.fault:
+          sys.stderr.write(server.fault['message'])
+        else:
+          sys.stderr.write("Unable to delete server '%s': task state is '%s' "
+                           "and current status is '%s'."
+                           % (server.name, task_state, server.status))
+        sys.exit(1)
+
+      if str(server.status) == "DELETED":
+        # wait to exit until server can no longer be found
+        log_deleting(seconds, task_state, server.status)
+        continue
+
+      if task_state is None and str(server.status) == "ACTIVE":
+        # wait as it takes a few seconds for the task_state to register deleting
+        log_deleting(seconds, task_state, server.status)
+        continue
+
+      if task_state != 'deleting':
+        sys.stderr.write("Unable to delete server '%s': task state is '%s' "
+                         "and current status is '%s'."
+                         % (server.name, task_state, server.status))
+        sys.exit(1)
+
+      log_deleting(seconds, task_state, server.status)
+  
+    except novaclient.exceptions.NotFound:
+      break
+
+def unmount_volumes(server, server_volumes, ssh_host_key_file, ssh_host):
+  # Note - unmounting volumes from a script that runs on localhost
+  # since delete scripts can run before the ssh-client address is known
+  #
+  if server.status == 'ACTIVE':
+    partitions = [ '%s1' % v.device for v in server_volumes ]
+  
+    opts = ("-o BatchMode=yes "
+            "-o UserKnownHostsFile='%s' " 
+            "-o ConnectTimeout=2") % ssh_host_key_file
+  
+    for v in server_volumes:
+      partname = "%s1" % v.device
+  
+      # check if volume mounted
+      cmd = "mount | grep -q '^%s '" % partname 
+      r = subprocess.call('ssh %s %s %s' % (opts, ssh_host, cmd), shell='true')
+      if r == 0:
+        # unmount it
+        cmd = "umount %s" % partname
+        r = subprocess.call('ssh %s %s %s' % (opts, ssh_host, cmd), shell='true')
+        if r != 0: # umount failed
+          sys.stderr.write("\nError unmounting '%s' for volume id '%s'." %
+                          (partname, v.id))
+          sys.exit(r)
 
 def detach_volume(server_id, volume_id):
   try:
@@ -127,9 +227,12 @@ def detach_volume(server_id, volume_id):
     volume = nova_volume.volumes.get(volume_id)
     if volume.status == 'available':
       break
+    if volume.status == 'error_detaching':
+      sys.stderr.write("error detaching '%s' volume" % volume_id)
+      sys.exit(1)
     else:
-      print ("detaching '%s' volume from '%s'... %s seconds" %
-            (volume_id, volume.device, seconds))
+      sys.stdout.write("detaching '%s' volume from '%s'... %s seconds" %
+                      (volume_id, volume.device, seconds))
       seconds += 2
       time.sleep(2)
 
@@ -142,7 +245,7 @@ def delete_volume(volume_id):
   while True:
     try:
       nova_volume.volumes.get(volume_id)
-      print "deleting server... %s seconds" % seconds
+      sys.stdout.write("deleting server... %s seconds" % seconds)
   
       # retry delete every 30 seconds
       if retry >= 30:
